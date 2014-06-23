9a30c583bf109f61893daf8e4b3ebda8b80ac177
mdeverdelhan <marc.deverdelhan@yahoo.com>
2014-06-23 15:45:52 +0200

    Adding double EMA and triple EMA indicators (still untested)

---
+++ ta4j/src/main/java/eu/verdelhan/ta4j/indicators/trackers/DoubleEMAIndicator.java
@@ -0,0 +1,47 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Marc de Verdelhan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package eu.verdelhan.ta4j.indicators.trackers;
+
+import eu.verdelhan.ta4j.Indicator;
+
+/**
+ * Double exponential moving average indicator.
+ * <p>
+ */
+public class DoubleEMAIndicator implements Indicator<Double> {
+
+    private final int timeFrame;
+
+    private final EMAIndicator ema;
+
+    public DoubleEMAIndicator(Indicator<? extends Number> indicator, int timeFrame) {
+        this.timeFrame = timeFrame;
+        this.ema = new EMAIndicator(indicator, timeFrame);
+    }
+
+    @Override
+    public Double getValue(int index) {
+        EMAIndicator emaEma = new EMAIndicator(ema, timeFrame);
+        return (2 * ema.getValue(index)) - emaEma.getValue(index);
+    }
+}
