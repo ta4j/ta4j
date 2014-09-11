504b27673cec4e485040143169ab9718f6b77206
mdeverdelhan <marc.deverdelhan@yahoo.com>
2014-09-11 15:43:22 +0200

    Updated license headers

--- ta4j/src/main/java/eu/verdelhan/ta4j/indicators/package-info.java
+++ ta4j/src/main/java/eu/verdelhan/ta4j/indicators/package-info.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Marc de Verdelhan
+ * Copyright (c) 2014 Marc de Verdelhan & respective authors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -20,7 +20,6 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 /**
  * Technical indicators.
  * <p>
