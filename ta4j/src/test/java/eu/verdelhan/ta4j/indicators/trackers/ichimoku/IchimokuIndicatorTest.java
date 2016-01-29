6fd69949fe6ad03951faf50a0daa8784407c46e4
gcauchis <gabriel.cauchis@gmail.com>
2016-01-29 13:51:22 +0100

    ichimoku add licences

--- ta4j/src/test/java/eu/verdelhan/ta4j/indicators/trackers/ichimoku/IchimokuIndicatorTest.java
+++ ta4j/src/test/java/eu/verdelhan/ta4j/indicators/trackers/ichimoku/IchimokuIndicatorTest.java
@@ -22,46 +22,25 @@
  */
 package eu.verdelhan.ta4j.indicators.trackers.ichimoku;
 
-import eu.verdelhan.ta4j.Decimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+
+import eu.verdelhan.ta4j.Tick;
 import eu.verdelhan.ta4j.TimeSeries;
-import eu.verdelhan.ta4j.indicators.CachedIndicator;
-import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
+import eu.verdelhan.ta4j.mocks.MockTimeSeries;
 
-/**
- * The Class IchimokuChikouSpanIndicator.
- */
-public class IchimokuChikouSpanIndicator extends CachedIndicator<Decimal> {
+public class IchimokuIndicatorTest {
 
-    /** The close price indicator. */
-    private final ClosePriceIndicator closePriceIndicator;
+    protected TimeSeries data;
     
-    /** The time delay. */
-    private final int timeDelay;
-    
-    /**
-     * Instantiates a new ichimoku chikou span indicator.
-     *
-     * @param series the series
-     */
-    public IchimokuChikouSpanIndicator(TimeSeries series) {
-        this(series, 26);
+    @Before
+    public void setUp() {
+        
+        List<Tick> ticks = new ArrayList<Tick>();
+//        ticks.add(new MockTick(11577.43, 11670.75, 11711.47, 11577.35));
+        
+        data = new MockTimeSeries(ticks);
     }
-    
-    /**
-     * Instantiates a new ichimoku chikou span indicator.
-     *
-     * @param series the series
-     * @param timeDelay the time delay (usually 26)
-     */
-    public IchimokuChikouSpanIndicator(TimeSeries series, int timeDelay) {
-        super(series);
-        closePriceIndicator = new ClosePriceIndicator(series);
-        this.timeDelay = timeDelay;
-    }
-
-    @Override
-    protected Decimal calculate(int index) {
-        return closePriceIndicator.getValue(Math.max(0, index - timeDelay));
-    }
-
 }
