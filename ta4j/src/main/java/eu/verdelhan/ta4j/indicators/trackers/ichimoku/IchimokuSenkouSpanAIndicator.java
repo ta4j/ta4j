26a4dbccac86dffa1c0ac0a3b9b2d3d68cfde65d
mdeverdelhan <marc.deverdelhan@yahoo.com>
2017-05-02 22:20:53 +0200

    Update license headers

--- ta4j/src/main/java/eu/verdelhan/ta4j/indicators/trackers/ichimoku/IchimokuSenkouSpanAIndicator.java
+++ ta4j/src/main/java/eu/verdelhan/ta4j/indicators/trackers/ichimoku/IchimokuSenkouSpanAIndicator.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2014-2016 Marc de Verdelhan & respective authors (see AUTHORS)
+ * Copyright (c) 2014-2017 Marc de Verdelhan & respective authors (see AUTHORS)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
