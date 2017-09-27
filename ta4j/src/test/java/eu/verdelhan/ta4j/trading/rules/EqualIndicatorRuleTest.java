94fa8bddef7e91388cf2eb1255e15a578050e92a
nimo23 <enegy@hotmail.com>
2017-09-27 20:27:07 +0200

    Update EqualIndicatorRuleTest.java

--- ta4j/src/test/java/eu/verdelhan/ta4j/trading/rules/EqualIndicatorRuleTest.java
+++ ta4j/src/test/java/eu/verdelhan/ta4j/trading/rules/EqualIndicatorRuleTest.java
@@ -20,7 +20,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package io.plan.ext.rule;
+package eu.verdelhan.ta4j.trading.rules;
 
 import eu.verdelhan.ta4j.trading.rules.EqualIndicatorRule;
 import eu.verdelhan.ta4j.Decimal;
