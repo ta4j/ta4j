bc42d8bd2d8d6aa0591a887cc864a97b0d286d02
nimo23 <enegy@hotmail.com>
2017-10-01 22:25:33 +0200

    Create InSlopeRuleTest

---
+++ ta4j/src/test/java/org/ta4j/core/trading/rules/InSlopeRuleTest.java
@@ -0,0 +1,57 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 Marc de Verdelhan & respective authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.trading.rules;
+
+import org.ta4j.core.Decimal;
+import org.ta4j.core.Indicator;
+import org.ta4j.core.indicators.helpers.FixedDecimalIndicator;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InSlopeRuleTest {
+
+    private Indicator<Decimal> indicator;
+    private InSlopeRule rule;
+    
+    @Before
+    public void setUp() {
+        indicator = new FixedDecimalIndicator(50, 70, 80, 90, 99, 60, 30, 20, 10, 0);
+        ruleSlopePositive = new InSlopeRule(indicator, Decimal.valueOf(20), Decimal.valueOf(30));
+        ruleSlopeNegative = new InSlopeRule(indicator, Decimal.valueOf(-20), Decimal.valueOf(-25));
+    }
+    
+    @Test
+    public void isSatisfied() {
+        assertFalse(ruleSlopePositive.isSatisfied(0));
+        assertTrue(ruleSlopePositive.isSatisfied(1));
+        assertFalse(ruleSlopePositive.isSatisfied(2));
+        assertFalse(ruleSlopePositive.isSatisfied(9));
+        
+        assertFalse(ruleSlopeNegative.isSatisfied(0));
+        assertFalse(ruleSlopeNegative.isSatisfied(1));
+        assertTrue(ruleSlopeNegative.isSatisfied(5));
+        assertFalse(ruleSlopeNegative.isSatisfied(9));
+    }
+}
