d552f5f858b3432e3acac062a1fcf805a03c878c
Simon W <simonjustuswimmer@googlemail.com>
2017-10-24 10:19:07 +0200

    Move indicators to ta4j-core package

--- ta4j/src/main/java/org/ta4j/core/indicators/pivotpoints/PivotLevel.java
+++
@@ -1,5 +0,0 @@
-package org.ta4j.core.indicators.pivotpoints;
-
-public enum PivotLevel {
-    RESISTANCE_3, RESISTANCE_2, RESISTANCE_1, SUPPORT_1, SUPPORT_2, SUPPORT_3
-}
--- ta4j/src/main/java/org/ta4j/core/indicators/pivotpoints/TimeLevel.java
+++
@@ -1,6 +0,0 @@
-package org.ta4j.core.indicators.pivotpoints;
-
-public enum TimeLevel {
-
-    TICKBASED, DAY, WEEK, MONTH, YEAR
-}
