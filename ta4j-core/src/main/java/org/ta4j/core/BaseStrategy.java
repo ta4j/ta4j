efb16b5e51f2b7bb23861141fd258d9141443c2f
nimo23 <enegy@hotmail.com>
2017-11-11 21:15:18 +0100

    Update BaseStrategy.java

--- ta4j-core/src/main/java/org/ta4j/core/BaseStrategy.java
+++ ta4j-core/src/main/java/org/ta4j/core/BaseStrategy.java
@@ -113,20 +113,10 @@ public class BaseStrategy implements Strategy {
     public Rule getExitRule() {
     	return exitRule;
     }
-    
+	
     @Override
-    public Strategy and(Strategy strategy) {
-    	return new BaseStrategy(entryRule.and(strategy.getEntryRule()), exitRule.and(strategy.getExitRule()));
-    }
-
-    @Override
-    public Strategy or(Strategy strategy) {
-    	return new BaseStrategy(entryRule.or(strategy.getEntryRule()), exitRule.or(strategy.getExitRule()));
-    }
-
-    @Override
-    public Strategy opposite(Strategy strategy) {
-    	return new BaseStrategy(exitRule, entryRule);
+    public int getUnstablePeriod() {
+    	return unstablePeriod;
     }
     
     @Override
@@ -151,6 +141,35 @@ public class BaseStrategy implements Strategy {
         boolean exit = Strategy.super.shouldExit(index, tradingRecord);
         traceShouldExit(index, exit);
         return exit;
+    }
+	
+    @Override
+    public Strategy and(Strategy strategy) {
+        String andName = "and(" + name + "," + strategy.getName() + ")";
+        int unstable = unstablePeriod > strategy.getUnstablePeriod() ? unstablePeriod : strategy.getUnstablePeriod();
+        return and(andName, strategy, unstable);
+    }
+
+    @Override
+    public Strategy or(Strategy strategy) {
+        String orName = "or(" + name + "," + strategy.getName() + ")";
+        int unstable = unstablePeriod > strategy.getUnstablePeriod() ? unstablePeriod : strategy.getUnstablePeriod();
+        return and(orName, strategy, unstable);
+    }
+
+    @Override
+    public Strategy opposite() {
+        return new BaseStrategy("opposite(" + name + ")", exitRule, entryRule, unstablePeriod);
+    }
+
+    @Override
+    public Strategy and(String name, Strategy strategy, int unstablePeriod) {
+        return new BaseStrategy(name, entryRule.and(strategy.getEntryRule()), exitRule.and(strategy.getExitRule()), unstablePeriod);
+    }
+
+    @Override
+    public Strategy or(String name, Strategy strategy, int unstablePeriod) {
+        return new BaseStrategy(name, entryRule.or(strategy.getEntryRule()), exitRule.or(strategy.getExitRule()), unstablePeriod);
     }
 
     /**
