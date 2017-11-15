4aa9fd8d16a47ba4c300e15d6a3a32bb0d92e95c
Athanasios Katsadimas <nkatsadim@gmail.com>
2017-11-15 21:52:19 +0200

    added unit test that proves multiplication symmetricity

--- ta4j-core/src/test/java/org/ta4j/core/DecimalTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/DecimalTest.java
@@ -2,12 +2,22 @@ package org.ta4j.core;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.*;
 
 
 public class DecimalTest {
     @Test
-    public void valueOf() {
+    public void testValueOf() {
         assertEquals(Decimal.valueOf(0.33), Decimal.valueOf("0.33"));
+    }
+
+    @Test
+    public void testMultiplicationSymmetricity(){
+        Decimal decimalFromString = Decimal.valueOf("0.33");
+        Decimal decimalFromDouble = Decimal.valueOf(0.33);
+
+        assertEquals(decimalFromString.multipliedBy(decimalFromDouble), decimalFromDouble.multipliedBy(decimalFromString));
     }
 }
