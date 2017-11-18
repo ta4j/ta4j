ce8189293fb1cc07d2064521f287c5995b250ac4
nimo23 <enegy@hotmail.com>
2017-11-18 19:10:58 +0100

    Update Indicator.java
    Add convenience "static" method "toDouble()" which returns a list of "Doubles" from a corresponding Indicator (based on decimal). With this, it is easy to use 3rd-party libs which only expect the data as double values (for example, commons-math or other mathematical libraries needs a double[] array as a input argument).

--- ta4j-core/src/main/java/org/ta4j/core/Indicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/Indicator.java
@@ -26,20 +26,46 @@ import java.io.Serializable;
 
 /**
  * Indicator over a {@link TimeSeries time series}.
- * <p></p>
+ * <p>
+ * </p>
  * For each index of the time series, returns a value of type <b>T</b>.
+ * 
  * @param <T> the type of returned value (Double, Boolean, etc.)
  */
 public interface Indicator<T> extends Serializable {
 
-    /**
-     * @param index the tick index
-     * @return the value of the indicator
-     */
-    T getValue(int index);
+	/**
+	 * @param index the tick index
+	 * @return the value of the indicator
+	 */
+	T getValue(int index);
 
-    /**
-     * @return the related time series
-     */
-    TimeSeries getTimeSeries();
+	/**
+	 * @return the related time series
+	 */
+	TimeSeries getTimeSeries();
+
+	/**
+	 * Returns all values from an {@link Indicator} as a Array of Doubles. The
+	 * returned doubles could have a minor loss of precise, if {@link Indicator}
+	 * was based on {@link Decimal Decimal}.
+	 * 
+	 * @param ref the indicator
+	 * @param index the index
+	 * @param timeFrame the timeFrame
+	 * @return array of double within the timeFrame
+	 */
+	static Double[] toDouble(Indicator<Decimal> ref, int index, int timeFrame) {
+
+		int end = Math.max(0, index - timeFrame + 1);
+		Double[] all = new Double[timeFrame];
+
+		for (int i = index - 1; i >= end; i--) {
+			Decimal number = ref.getValue(index);
+			all[i] = number.toDouble();
+		}
+
+		return all;
+	}
+
 }
