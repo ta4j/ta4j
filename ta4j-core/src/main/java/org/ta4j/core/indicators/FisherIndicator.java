28c5a9b6f600d5bc2b6b476f7973e7389250debf
nimo23 <enegy@hotmail.com>
2017-11-26 18:47:37 +0100

    Update FisherIndicator.java

--- ta4j-core/src/main/java/org/ta4j/core/indicators/FisherIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/FisherIndicator.java
@@ -200,7 +200,7 @@ public class FisherIndicator extends RecursiveCachedIndicator<Decimal> {
     }
 
     // Fisher = gamma * Log((1 + Value) / (1 - Value)) + delta * priorFisher
-    Decimal term1 = Decimal.ONE.plus(value).dividedBy(Decimal.ONE.minus(value)).log();
+    Decimal term1 = Decimal.valueOf((Math.log(Decimal.ONE.plus(value).dividedBy(Decimal.ONE.minus(value)).toDouble())));
     Decimal term2 = getValue(index - 1);
     return gamma.multipliedBy(term1).plus(delta.multipliedBy(term2));
     }
