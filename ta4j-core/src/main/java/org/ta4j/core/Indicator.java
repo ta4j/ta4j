74f1af9eb015d72210a2f87d7cd25e6440329562
nimo23 <enegy@hotmail.com>
2017-11-27 15:55:10 +0100

    Update Indicator.java

--- ta4j-core/src/main/java/org/ta4j/core/Indicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/Indicator.java
@@ -56,11 +56,10 @@ public interface Indicator<T> extends Serializable {
 	 * @return array of double within the timeFrame
 	 */
 	static Double[] toDouble(Indicator<Decimal> ref, int index, int timeFrame) {
-
-		int end = Math.max(0, index - timeFrame + 1);
+		
 		Double[] all = new Double[timeFrame];
 
-		for (int i = index - 1; i >= end; i--) {
+		for (int i = Math.max(0, index - timeFrame + 1); i <= index; i++) {
 			Decimal number = ref.getValue(index);
 			all[i] = number.toDouble();
 		}
