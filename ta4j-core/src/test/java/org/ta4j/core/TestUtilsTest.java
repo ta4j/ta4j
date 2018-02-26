7aebd005205fb90c52d2526df27c6639ee5f33f5
edlins <edlins@yahoo.com>
2018-02-26 11:58:19 -0700

    TestUtilsTest: fixed all cases to inexact matches, call tests with different precisions (force fail) TestUtils: add setPrecision(String)

--- ta4j-core/src/test/java/org/ta4j/core/TestUtilsTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/TestUtilsTest.java
@@ -11,6 +11,7 @@ import static org.ta4j.core.TestUtils.assertNumEquals;
 import static org.ta4j.core.TestUtils.assertNumNotEquals;
 import static org.ta4j.core.TestUtils.assertIndicatorEquals;
 import static org.ta4j.core.TestUtils.assertIndicatorNotEquals;
+import static org.ta4j.core.TestUtils.setPrecision;
 
 public class TestUtilsTest extends AbstractIndicatorTest {
 
@@ -19,67 +20,99 @@ public class TestUtilsTest extends AbstractIndicatorTest {
     }
 
     @Test
-    public void testStringNum() {
-        // exact matches only
-        assertNumNotEquals("0.1234", numOf(0.123456789));
+    public void testStringNumPass() {
+        stringNum("0.0001");
+    }
+
+    @Test(expected = java.lang.AssertionError.class)
+    public void testStringNumFail() {
+        stringNum("0.00001");
+    }
+
+    public void stringNum(String precision) {
+        setPrecision(precision);
+        assertNumEquals("0.1234", numOf(0.123456789));
         assertNumNotEquals("0.123", numOf(0.123456789));
-        assertNumNotEquals("0.123456789", numOf(0.1234));
+        assertNumEquals("0.123456789", numOf(0.1234));
         assertNumNotEquals("0.123456789", numOf(0.123));
         assertNumEquals("0.123456789", numOf(0.123456789));
-        // fails for BigDecimalNum only
-        //assertNumEquals("0.12345678", numOf(0.123456789));
-        // fails for DoubleNum only
-        //assertNumNotEquals("0.12345", numOf(0.123456789));
-        assertNumNotEquals("0.12345", numOf(0.12344));
+        assertNumEquals("0.12345678", numOf(0.123456789));
+        assertNumEquals("0.12345", numOf(0.123456789));
+        assertNumEquals("0.12345", numOf(0.12344));
         assertNumNotEquals("0.12345", numOf(0.123));
-        assertNumNotEquals("0.12344", numOf(0.12345));
+        assertNumEquals("0.12344", numOf(0.12345));
         assertNumNotEquals("0.123", numOf(0.12345));
     }
 
     @Test
-    public void testDoubleNum() {
-        // inexact matches allowed
-        assertNumEquals(0.1234, numOf(0.123456789)); // different from testStringNum!
+    public void testDoubleNumPass() {
+        doubleNum("0.0001");
+    }
+
+    @Test(expected = java.lang.AssertionError.class)
+    public void testDoubleNumFail() {
+        doubleNum("0.00001");
+    }
+
+    public void doubleNum(String precision) {
+        setPrecision(precision);
+        assertNumEquals(0.1234, numOf(0.123456789));
         assertNumNotEquals(0.123, numOf(0.123456789));
-        assertNumEquals(0.123456789, numOf(0.1234)); // different from testStringNum!
+        assertNumEquals(0.123456789, numOf(0.1234));
         assertNumNotEquals(0.123456789, numOf(0.123));
         assertNumEquals(0.123456789, numOf(0.123456789));
-        assertNumEquals(0.12345678, numOf(0.123456789)); // different from testStringNum!
-        assertNumEquals(0.12345, numOf(0.123456789)); // different from testStringNum!
-        assertNumEquals(0.12345, numOf(0.12344)); // different from testStringNum!
+        assertNumEquals(0.12345678, numOf(0.123456789));
+        assertNumEquals(0.12345, numOf(0.123456789));
+        assertNumEquals(0.12345, numOf(0.12344));
         assertNumNotEquals(0.12345, numOf(0.123));
-        assertNumEquals(0.12344, numOf(0.12345)); // different from testStringNum!
+        assertNumEquals(0.12344, numOf(0.12345));
         assertNumNotEquals(0.123, numOf(0.12345));
     }
 
     @Test
-    public void testNumNum() {
-        // exact matches only (identical results to testStringNum)
-        assertNumNotEquals(numOf(0.1234), numOf(0.123456789));
+    public void testNumNumPass() {
+        numNum("0.0001");
+    }
+
+    @Test(expected = java.lang.AssertionError.class)
+    public void testNumNumFail() {
+        numNum("0.00001");
+    }
+
+    public void numNum(String precision) {
+        setPrecision(precision);
+        assertNumEquals(numOf(0.1234), numOf(0.123456789));
         assertNumNotEquals(numOf(0.123), numOf(0.123456789));
-        assertNumNotEquals(numOf(0.123456789), numOf(0.1234));
+        assertNumEquals(numOf(0.123456789), numOf(0.1234));
         assertNumNotEquals(numOf(0.123456789), numOf(0.123));
         assertNumEquals(numOf(0.123456789), numOf(0.123456789));
-        // fails for BigDecimalNum only
-        //assertNumEquals(numOf(0.12345678), numOf(0.123456789));
-        // fails for DoubleNum only
-        //assertNumNotEquals(numOf(0.12345), numOf(0.123456789));
-        assertNumNotEquals(numOf(0.12345), numOf(0.12344));
+        assertNumEquals(numOf(0.12345678), numOf(0.123456789));
+        assertNumEquals(numOf(0.12345), numOf(0.123456789));
+        assertNumEquals(numOf(0.12345), numOf(0.12344));
         assertNumNotEquals(numOf(0.12345), numOf(0.123));
-        assertNumNotEquals(numOf(0.12344), numOf(0.12345));
+        assertNumEquals(numOf(0.12344), numOf(0.12345));
         assertNumNotEquals(numOf(0.123), numOf(0.12345));
     }
 
     @Test
-    public void testIndicatorIndicator() {
-        // inexact matches allowed
-        assertIndicatorEquals( // different from testStringNum!
+    public void testIndicatorIndicatorPass() {
+        indicatorIndicator("0.0001");
+    }
+
+    @Test(expected = java.lang.AssertionError.class)
+    public void testIndicatorIndicatorFail() {
+        indicatorIndicator("0.00001");
+    }
+
+    public void indicatorIndicator(String precision) {
+        setPrecision(precision);
+        assertIndicatorEquals(
                 new MockIndicator(numFunction, numOf(0.1234)),
                 new MockIndicator(numFunction, numOf(0.123456789)));
         assertIndicatorNotEquals(
                 new MockIndicator(numFunction, numOf(0.123)),
                 new MockIndicator(numFunction, numOf(0.123456789)));
-        assertIndicatorEquals( // different from testStringNum!
+        assertIndicatorEquals(
                 new MockIndicator(numFunction, numOf(0.123456789)),
                 new MockIndicator(numFunction, numOf(0.1234)));
         assertIndicatorNotEquals(
@@ -88,19 +121,19 @@ public class TestUtilsTest extends AbstractIndicatorTest {
         assertIndicatorEquals(
                 new MockIndicator(numFunction, numOf(0.123456789)),
                 new MockIndicator(numFunction, numOf(0.123456789)));
-        assertIndicatorEquals( // different from testStringNum!
+        assertIndicatorEquals(
                 new MockIndicator(numFunction, numOf(0.12345678)),
                 new MockIndicator(numFunction, numOf(0.123456789)));
-        assertIndicatorEquals( // different from testStringNum!
+        assertIndicatorEquals(
                 new MockIndicator(numFunction, numOf(0.12345)),
                 new MockIndicator(numFunction, numOf(0.123456789)));
-        assertIndicatorEquals( // different from testStringNum!
+        assertIndicatorEquals(
                 new MockIndicator(numFunction, numOf(0.12345)),
                 new MockIndicator(numFunction, numOf(0.12344)));
         assertIndicatorNotEquals(
                 new MockIndicator(numFunction, numOf(0.12345)),
                 new MockIndicator(numFunction, numOf(0.123)));
-        assertIndicatorEquals( // different from testStringNum!
+        assertIndicatorEquals(
                 new MockIndicator(numFunction, numOf(0.12344)),
                 new MockIndicator(numFunction, numOf(0.12345)));
         assertIndicatorNotEquals(
