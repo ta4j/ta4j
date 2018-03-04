83f95de11783b2dfb6b318356d2cbedea3439126
edlins <edlins@yahoo.com>
2018-03-04 16:48:47 -0700

    fix comment

--- ta4j-core/src/test/java/org/ta4j/core/TestUtils.java
+++ ta4j-core/src/test/java/org/ta4j/core/TestUtils.java
@@ -54,7 +54,7 @@ public class TestUtils {
      *
      * @param actual the actual {@code Num} value
      * @param expected the given {@code Num} representation to compare the actual value to
-     * @throws AssertionError if the actual value is not equal to the given {@code String} representation
+     * @throws AssertionError if the actual value is not equal to the given {@code Num} representation
      */
     public static void assertNumEquals(Num expected, Num actual){
         assertEquals(expected, actual);
