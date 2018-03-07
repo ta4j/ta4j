ef247e7430690829e2e38b4e2b3ccef4709ec87a
edlins <edlins@yahoo.com>
2018-03-07 16:01:28 -0700

    access statically

--- ta4j-examples/src/test/java/ta4jexamples/bots/TradingBotOnMovingTimeSeriesTest.java
+++ ta4j-examples/src/test/java/ta4jexamples/bots/TradingBotOnMovingTimeSeriesTest.java
@@ -4,11 +4,9 @@ import org.junit.Test;
 
 public class TradingBotOnMovingTimeSeriesTest {
 
-    TradingBotOnMovingTimeSeries bot = new TradingBotOnMovingTimeSeries();
-
     @Test
     public void test() throws InterruptedException {
-        bot.main(new String[0]);
+        TradingBotOnMovingTimeSeries.main(new String[0]);
     }
 
 }
