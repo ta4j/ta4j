589172a672d89a27e585799658240d37123ffac8
Mate Tamas <GDI.MateTamas@gmail.com>
2018-03-26 22:59:42 +0200

    TripleEma comment refreshed

--- ta4j-core/src/main/java/org/ta4j/core/indicators/TripleEMAIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/TripleEMAIndicator.java
@@ -61,7 +61,7 @@ public class TripleEMAIndicator extends CachedIndicator<Num> {
     @Override
     protected Num calculate(int index) {
     	
-    		// trix = 3 * ema - 3 * emaEma + 3 * emaEmaEma
+    		// trix = 3 * ( ema - emaEma ) + emaEmaEma
         return numOf(3).multipliedBy(
         		ema.getValue(index)
         			.minus(emaEma.getValue(index)))
