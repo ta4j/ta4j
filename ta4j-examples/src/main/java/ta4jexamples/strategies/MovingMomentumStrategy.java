731ea1430afb453dfe3a8c58b5f99c5250787ad1
Chr1st0ph <cpreissner@gmx.de>
2018-04-04 10:43:09 +0200

    fixed link

--- ta4j-examples/src/main/java/ta4jexamples/strategies/MovingMomentumStrategy.java
+++ ta4j-examples/src/main/java/ta4jexamples/strategies/MovingMomentumStrategy.java
@@ -37,8 +37,8 @@ import ta4jexamples.loaders.CsvTradesLoader;
 /**
  * Moving momentum strategy.
  * <p></p>
- * @see <a href="http://stockcharts.com/help/doku.php?id=chart_school:trading_strategies:moving_momentum">
- *     http://stockcharts.com/help/doku.php?id=chart_school:trading_strategies:moving_momentum</a>
+ * @see <a href="http://stockcharts.com/school/doku.php?id=chart_school:trading_strategies:moving_momentum">
+ *     http://stockcharts.com/school/doku.php?id=chart_school:trading_strategies:moving_momentum</a>
  */
 public class MovingMomentumStrategy {
 
