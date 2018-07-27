0a9301b27130049e6cf147b5110ca7ae5b9c4285
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2018-07-27 12:30:40 +0200

    Now able to backtest on one series and trade on another series

--- ta4j-core/src/main/java/org/ta4j/core/Backtesting.java
+++ ta4j-core/src/main/java/org/ta4j/core/Backtesting.java
@@ -9,16 +9,20 @@ import java.util.List;
 
 public class Backtesting {
 
-    private TimeSeries series;
+    private TimeSeries seriesToTradeOn;
     private AbstractAnalysisCriterion criterion;
     private TimeSeriesManager seriesManager;
     private List<Strategy> strategies = new ArrayList<>();
     private List<BacktestingResult> backtestingResults = new ArrayList<>();
 
     public Backtesting(TimeSeries series, AbstractAnalysisCriterion criterion) {
-        this.series = series;
+        this(series, series, criterion);
+    }
+
+    public Backtesting(TimeSeries seriesToBacktest, TimeSeries seriesToTradeOn, AbstractAnalysisCriterion criterion) {
+        this.seriesToTradeOn = seriesToTradeOn;
         this.criterion = criterion;
-        this.seriesManager = new TimeSeriesManager(series);
+        this.seriesManager = new TimeSeriesManager(seriesToBacktest);
     }
 
     public void addStrategy(Strategy strategy) {
@@ -32,7 +36,7 @@ public class Backtesting {
     public void calculate(Num amount, PriceType priceType) {
         for(Strategy strategy : strategies) {
             TradingRecord tradingRecord = seriesManager.run(strategy, Order.OrderType.BUY, amount, priceType);
-            Num calculation = criterion.calculate(series, tradingRecord);
+            Num calculation = criterion.calculate(seriesToTradeOn, tradingRecord);
             backtestingResults.add(new BacktestingResult(strategy.getName(), strategy, tradingRecord, calculation));
         }
     }
@@ -40,7 +44,7 @@ public class Backtesting {
     public void printBacktestingResults(boolean printTrades) {
         Collections.sort(backtestingResults);
         for(BacktestingResult backtestingResult : backtestingResults) {
-            backtestingResult.printBacktestingResult(printTrades, series);
+            backtestingResult.printBacktestingResult(printTrades, seriesToTradeOn);
         }
     }
 }
