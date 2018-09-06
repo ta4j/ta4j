7f2f5770ffa1ef3fd6dc81e334ba57cc6ab70285
Simon W <simonjustuswimmer@googlemail.com>
2018-09-06 20:13:46 +0200

    Fixed RandomWalkIndicators, added Excel-Sheet validation (#314)
    renamed to RWI[High|Low]Indicator

--- ta4j-core/src/test/java/org/ta4j/core/indicators/RWIHighIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/RWIHighIndicatorTest.java
@@ -23,50 +23,34 @@
  *******************************************************************************/
 package org.ta4j.core.indicators;
 
+import static org.ta4j.core.TestUtils.assertIndicatorEquals;
+
+import java.util.function.Function;
+
+import org.junit.Test;
+import org.ta4j.core.ExternalIndicatorTest;
 import org.ta4j.core.TimeSeries;
-import org.ta4j.core.indicators.helpers.MaxPriceIndicator;
-import org.ta4j.core.indicators.helpers.MinPriceIndicator;
 import org.ta4j.core.num.Num;
 
 /**
- * The Class RandomWalkIndexHighIndicator.
+ * Testing the RWIHighIndicator
  */
-public class RandomWalkIndexHighIndicator extends CachedIndicator<Num> {
+public class RWIHighIndicatorTest extends AbstractIndicatorTest<TimeSeries,Num>{
 
-    private final MaxPriceIndicator maxPrice;
-    
-    private final MinPriceIndicator minPrice;
-    
-    private final ATRIndicator averageTrueRange;
-    
-    private final Num sqrtBarCount;
-    
-    private final int barCount;
-    
-    /**
-     * Constructor.
-     *
-     * @param series the series
-     * @param barCount the time frame
-     */
-    public RandomWalkIndexHighIndicator(TimeSeries series, int barCount) {
-        super(series);
-        this.barCount = barCount;
-        maxPrice = new MaxPriceIndicator(series);
-        minPrice = new MinPriceIndicator(series);
-        averageTrueRange = new ATRIndicator(series, barCount);
-        sqrtBarCount = numOf(barCount).sqrt();
+	/**
+	 * TODO: Just graphically Excel-Sheet validation with hard coded results. Excel formula needed
+	 */
+	private ExternalIndicatorTest xls;
+
+    public RWIHighIndicatorTest(Function<Number, Num> numFunction) {
+        super((data, params) -> new RWIHighIndicator(data, (int) params[0]), numFunction);
+        xls = new XLSIndicatorTest(this.getClass(), "RWIHL.xls", 8, numFunction);
     }
 
-    @Override
-    protected Num calculate(int index) {
-        return maxPrice.getValue(index).minus(minPrice.getValue(Math.max(0, index - barCount)))
-                .dividedBy(averageTrueRange.getValue(index).multipliedBy(sqrtBarCount));
+    @Test
+    public void randomWalkIndexHigh() throws Exception{
+    	TimeSeries series = xls.getSeries();
+        RWIHighIndicator rwih = (RWIHighIndicator) getIndicator(series, 20);
+        assertIndicatorEquals(getIndicator(series, 20), rwih);
     }
-    
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + " barCount: " + barCount;
-    }
-
 }
