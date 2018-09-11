592ec9c01be381026012509fdae6ab04b4ed9fc9
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2018-09-11 19:23:52 +0200

    Now comparing profit/loss performance in percentage when backtesting

--- ta4j-core/src/main/java/org/ta4j/core/BacktestingResult.java
+++ ta4j-core/src/main/java/org/ta4j/core/BacktestingResult.java
@@ -11,6 +11,7 @@ public class BacktestingResult implements Comparable<BacktestingResult> {
     private Strategy strategy;
     private TradingRecord tradingRecord;
     private Num totalProfitLoss;
+    private Num totalProfitLossPercentage;
     private Num totalProfit;
     private Num totalLoss;
     private Num profitTradeCount;
@@ -21,6 +22,7 @@ public class BacktestingResult implements Comparable<BacktestingResult> {
         this.strategy = strategy;
         this.tradingRecord = tradingRecord;
         this.totalProfitLoss = new ProfitLossCriterion().calculate(series, tradingRecord);
+        this.totalProfitLossPercentage = new ProfitLossPercentageCriterion().calculate(series, tradingRecord);
         this.totalProfit = new TotalProfit2Criterion().calculate(series, tradingRecord);
         this.totalLoss = new TotalLossCriterion().calculate(series, tradingRecord);
         this.profitTradeCount = new NumberOfWinningTradesCriterion().calculate(series, tradingRecord);
@@ -44,6 +46,10 @@ public class BacktestingResult implements Comparable<BacktestingResult> {
         return totalProfitLoss;
     }
 
+    public Num getTotalProfitLossPercentage() {
+        return totalProfitLossPercentage;
+    }
+
     /**
      * Print to console the backtesting result
      *
@@ -57,6 +63,7 @@ public class BacktestingResult implements Comparable<BacktestingResult> {
         System.out.println("Total loss: " + totalLoss + " Trade count: " + lossTradeCount);
         System.out.println("Break event trade count: " + breakEvenTradeCount);
         System.out.println("Total profitLoss: " + totalProfitLoss);
+        System.out.println("Total profitLoss: " + round(totalProfitLossPercentage, 1) + "%");
         if (printTrades) {
             tradingRecord.getTrades().stream().forEach(trade -> printTrade(trade, series));
             Order lastOrder = tradingRecord.getLastOrder();
@@ -76,6 +83,11 @@ public class BacktestingResult implements Comparable<BacktestingResult> {
 
     @Override
     public int compareTo(BacktestingResult that) {
-        return this.totalProfitLoss.compareTo(that.getTotalProfitLoss());
+        return this.totalProfitLossPercentage.compareTo(that.getTotalProfitLossPercentage());
+    }
+
+    private static double round(Num value, int precision) {
+        int scale = (int) Math.pow(10, precision);
+        return (double) Math.round(value.doubleValue() * scale) / scale;
     }
 }
