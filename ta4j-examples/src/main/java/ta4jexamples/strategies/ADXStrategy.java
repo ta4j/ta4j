9c7e16e8f37f11cb042a17f965b31ca8ea04da67
Bartosz Milczarek <bartosz@milczarek.it>
2018-10-14 22:37:17 +0200

    Added to examples ADX indicator based strategy (#336)
    * Added to examples ADX indicator based strategy described here: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:average_directional_index_adx
    
    * Added entry to CHANGELOG.md

---
+++ ta4j-examples/src/main/java/ta4jexamples/strategies/ADXStrategy.java
@@ -0,0 +1,76 @@
+package ta4jexamples.strategies;
+
+import org.ta4j.core.BaseStrategy;
+import org.ta4j.core.Rule;
+import org.ta4j.core.Strategy;
+import org.ta4j.core.TimeSeries;
+import org.ta4j.core.TimeSeriesManager;
+import org.ta4j.core.TradingRecord;
+import org.ta4j.core.analysis.criteria.TotalProfitCriterion;
+import org.ta4j.core.indicators.SMAIndicator;
+import org.ta4j.core.indicators.adx.ADXIndicator;
+import org.ta4j.core.indicators.adx.MinusDIIndicator;
+import org.ta4j.core.indicators.adx.PlusDIIndicator;
+import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
+import org.ta4j.core.trading.rules.CrossedDownIndicatorRule;
+import org.ta4j.core.trading.rules.CrossedUpIndicatorRule;
+import org.ta4j.core.trading.rules.OverIndicatorRule;
+import org.ta4j.core.trading.rules.UnderIndicatorRule;
+import ta4jexamples.loaders.CsvTradesLoader;
+
+/**
+ * ADX indicator based strategy
+ * </p>
+ *
+ * @see <a href="http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:average_directional_index_adx">
+ * http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:average_directional_index_adx</a>
+ */
+public class ADXStrategy {
+
+    /**
+     * @param series a time series
+     * @return an adx indicator based strategy
+     */
+    public static Strategy buildStrategy(TimeSeries series) {
+        if (series == null) {
+            throw new IllegalArgumentException("Series cannot be null");
+        }
+
+        final ClosePriceIndicator closePriceIndicator = new ClosePriceIndicator(series);
+        final SMAIndicator smaIndicator = new SMAIndicator(closePriceIndicator, 50);
+
+        final int adxBarCount = 14;
+        final ADXIndicator adxIndicator = new ADXIndicator(series, adxBarCount);
+        final OverIndicatorRule adxOver20Rule = new OverIndicatorRule(adxIndicator, 20);
+
+        final PlusDIIndicator plusDIIndicator = new PlusDIIndicator(series, adxBarCount);
+        final MinusDIIndicator minusDIIndicator = new MinusDIIndicator(series, adxBarCount);
+
+        final Rule plusDICrossedUpMinusDI = new CrossedUpIndicatorRule(plusDIIndicator, minusDIIndicator);
+        final Rule plusDICrossedDownMinusDI = new CrossedDownIndicatorRule(plusDIIndicator, minusDIIndicator);
+        final OverIndicatorRule closePriceOverSma = new OverIndicatorRule(closePriceIndicator, smaIndicator);
+        final Rule entryRule = adxOver20Rule.and(plusDICrossedUpMinusDI).and(closePriceOverSma);
+
+        final UnderIndicatorRule closePriceUnderSma = new UnderIndicatorRule(closePriceIndicator, smaIndicator);
+        final Rule exitRule = adxOver20Rule.and(plusDICrossedDownMinusDI).and(closePriceUnderSma);
+
+        return new BaseStrategy("ADX", entryRule, exitRule, adxBarCount);
+    }
+
+    public static void main(String[] args) {
+
+        // Getting the time series
+        TimeSeries series = CsvTradesLoader.loadBitstampSeries();
+
+        // Building the trading strategy
+        Strategy strategy = buildStrategy(series);
+
+        // Running the strategy
+        TimeSeriesManager seriesManager = new TimeSeriesManager(series);
+        TradingRecord tradingRecord = seriesManager.run(strategy);
+        System.out.println("Number of trades for the strategy: " + tradingRecord.getTradeCount());
+
+        // Analysis
+        System.out.println("Total profit for the strategy: " + new TotalProfitCriterion().calculate(series, tradingRecord));
+    }
+}
