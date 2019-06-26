108869772d450d8a61d4213f79aaa5f3f62ba080
nimo23 <enegy@hotmail.com>
2019-06-27 00:04:08 +0200

    Update BaseTimeSeries.java
    Add new methods for bar aggregation (upscaling):
    
    - aggregateBars
    - aggregateTimeSeries

--- ta4j-core/src/main/java/org/ta4j/core/BaseTimeSeries.java
+++ ta4j-core/src/main/java/org/ta4j/core/BaseTimeSeries.java
@@ -422,6 +422,105 @@ public class BaseTimeSeries implements TimeSeries {
     private static List<Bar> cut(List<Bar> bars, final int startIndex, final int endIndex){
         return new ArrayList<>(bars.subList(startIndex, endIndex));
     }
+    
+	/**
+	 * Aggregates the bars of the <code>timeSeries</code> by
+	 * <code>timePeriod</code>. The new <code>timePeriod</code> must be a
+	 * multiplication of the actual time period.
+	 * 
+	 * @param timeSeries the actual TimeSeries
+	 * @param timePeriod the actual timePeriod
+	 * @return the aggregated TimeSeries
+	 */
+	public static TimeSeries aggregateTimeSeries(TimeSeries timeSeries, Duration timePeriod) {
+		String name = timeSeries.getName() + "_upscaled_to_" + timePeriod;
+		return aggregateTimeSeries(name, timeSeries, timePeriod);
+	}
+
+	/**
+	 * Aggregates the bars of the <code>timeSeries</code> by
+	 * <code>timePeriod</code>. The new <code>timePeriod</code> must be a
+	 * multiplication of the actual time period.
+	 * 
+	 * @param name       the name of the returned TimeSeries
+	 * @param timeSeries the actual TimeSeries
+	 * @param timePeriod the actual timePeriod
+	 * @return the aggregated TimeSeries
+	 */
+	public static TimeSeries aggregateTimeSeries(String name, TimeSeries timeSeries, Duration timePeriod) {
+		List<Bar> sumBars = aggregateBars(timeSeries.getBarData(), timePeriod);
+		return new BaseTimeSeries(name, sumBars);
+	}
+
+	/**
+	 * Aggregates a list of bars by <code>timePeriod</code>.The new
+	 * <code>timePeriod</code> must be a multiplication of the actual time period.
+	 * 
+	 * 
+	 * @param bars       the actual bars
+	 * @param timePeriod the new timePeriod
+	 * @return the aggregated bars with new <code>timePeriod</code>
+	 */
+	public static List<Bar> aggregateBars(List<Bar> bars, Duration timePeriod) {
+
+		List<Bar> sumBars = new ArrayList<>();
+
+		if (bars.isEmpty()) {
+			return sumBars;
+		}
+
+		// get the actual time period
+		Duration actualDur = bars.iterator().next().getTimePeriod();
+
+		// check if new timePeriod is a multiplication of actual time period
+		boolean isNotMultiplication = timePeriod.getSeconds() % actualDur.getSeconds() != 0;
+		
+		if (isNotMultiplication) {
+			throw new IllegalArgumentException(
+					"Cannot aggregate bars: the new timePeriod must be a multiplication of the actual timePeriod.");
+		}
+
+		int i = 0;
+		while (i < bars.size()) {
+			Bar b1 = bars.get(i);
+			ZonedDateTime beginTime = b1.getBeginTime();
+			Num open = b1.getOpenPrice();
+			Num max = b1.getMaxPrice();
+			Num min = b1.getMinPrice();
+			
+			// set to ZERO
+			Num close = b1.getOpenPrice().numOf(0);
+			Num volume = b1.getOpenPrice().numOf(0);
+			Num amount = b1.getOpenPrice().numOf(0);
+			Duration sumDur = Duration.ZERO;
+
+			while (sumDur.compareTo(timePeriod) < 0) {
+				if (i < bars.size()) {
+					Bar b2 = bars.get(i);
+
+					if (b2.getMaxPrice().isGreaterThan(max)) {
+						max = b2.getMaxPrice();
+					}
+					if (b2.getMinPrice().isLessThan(min)) {
+						min = b2.getMinPrice();
+					}
+					close = b2.getClosePrice();
+					volume = volume.plus(b2.getVolume());
+					amount = amount.plus(b2.getAmount());
+				}
+				sumDur = sumDur.plus(actualDur);
+				i++;
+			}
+
+			// add only bars with elapsed timePeriod
+			if (i <= bars.size()) {
+				Bar b = new BaseBar(timePeriod, beginTime.plus(timePeriod), open, max, min, close, volume, amount);
+				sumBars.add(b);
+			}
+		}
+
+		return sumBars;
+	}
 
     /**
      * @param series a time series
