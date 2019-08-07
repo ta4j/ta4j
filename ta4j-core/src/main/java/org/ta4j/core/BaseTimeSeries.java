80851456a048b26098c207cf482536e8ff47632e
nimo23 <enegy@hotmail.com>
2019-08-07 11:43:18 +0200

    Update BaseTimeSeries.java
    correct code formatting

--- ta4j-core/src/main/java/org/ta4j/core/BaseTimeSeries.java
+++ ta4j-core/src/main/java/org/ta4j/core/BaseTimeSeries.java
@@ -467,58 +467,58 @@ public class BaseTimeSeries implements TimeSeries {
         return replaceBarIfChanged(this.getBarData(), newBar);
     }
  
-	/**
-	 * @param bars   the existing bars
-	 * @param newBar the bar which has precedence over same existing bar
-	 * @return the previous bar replaced by newBar, or null if there was no
-	 *         replacement.
-	 * @see #replaceBarIfChanged(Bar)
-	 */
-	private Bar replaceBarIfChanged(List<Bar> bars, Bar newBar) {
+    /**
+    * @param bars   the existing bars
+    * @param newBar the bar which has precedence over same existing bar
+    * @return the previous bar replaced by newBar, or null if there was no
+    *         replacement.
+    * @see #replaceBarIfChanged(Bar)
+    */
+    private Bar replaceBarIfChanged(List<Bar> bars, Bar newBar) {
         if (bars == null || bars.isEmpty()) return null;
         for (int i = 0; i < bars.size(); i++) {
-                Bar bar = bars.get(i);
-                boolean isSameBar = bar.getBeginTime().isEqual(newBar.getBeginTime())
-                && bar.getEndTime().isEqual(newBar.getEndTime())
-                && bar.getTimePeriod().equals(newBar.getTimePeriod());
-                if (isSameBar && !bar.equals(newBar)) return bars.set(i, newBar);
+          Bar bar = bars.get(i);
+          boolean isSameBar = bar.getBeginTime().isEqual(newBar.getBeginTime())
+          && bar.getEndTime().isEqual(newBar.getEndTime())
+          && bar.getTimePeriod().equals(newBar.getTimePeriod());
+          if (isSameBar && !bar.equals(newBar)) return bars.set(i, newBar);
         }
 		
         return null;
-	}
+    }
 
-	@Override
-	public List<ZonedDateTime> findMissingBars() {
+    @Override
+    public List<ZonedDateTime> findMissingBars() {
         return findMissingBars(this.getBarData());
-	}
+    }
 
-	/**
-	 * Finds potential missing bars.
-	 * 
-	 * @param bars the list of bars
-	 * @return the list of missing bars
-	 * @see #findMissingBars()
-	 */
-	private List<ZonedDateTime> findMissingBars(List<Bar> bars) {
+    /**
+    * Finds potential missing bars.
+    * 
+    * @param bars the list of bars
+    * @return the list of missing bars
+    * @see #findMissingBars()
+     */
+    private List<ZonedDateTime> findMissingBars(List<Bar> bars) {
         if (bars == null || bars.isEmpty()) return List.of();
         Duration duration = bars.iterator().next().getTimePeriod();
         List<ZonedDateTime> missingBars = new ArrayList<>();
         for (int i = 0; i < bars.size(); i++) {
-                Bar bar = bars.get(i);
-                Bar nextBar = i + 1 < bars.size() ? bars.get(i + 1) : null;
-                Duration incDuration = Duration.ZERO;
-                if (nextBar != null) {
-                    while (nextBar.getBeginTime().minus(incDuration).isAfter(bar.getEndTime())) {
-                        missingBars.add(bar.getEndTime().plus(incDuration));
-                        incDuration = incDuration.plus(duration);
-                    }
-                }
-                boolean noFullData = bar.getOpenPrice().isNaN() || bar.getHighPrice().isNaN() || bar.getLowPrice().isNaN();
-                if (noFullData) {
-                        missingBars.add(bar.getBeginTime());
-                }
+          Bar bar = bars.get(i);
+          Bar nextBar = i + 1 < bars.size() ? bars.get(i + 1) : null;
+          Duration incDuration = Duration.ZERO;
+          if (nextBar != null) {
+              while (nextBar.getBeginTime().minus(incDuration).isAfter(bar.getEndTime())) {
+                  missingBars.add(bar.getEndTime().plus(incDuration));
+                  incDuration = incDuration.plus(duration);
+              }
+          }
+          boolean noFullData = bar.getOpenPrice().isNaN() || bar.getHighPrice().isNaN() || bar.getLowPrice().isNaN();
+          if (noFullData) {
+              missingBars.add(bar.getBeginTime());
+          }
         }
 		
         return missingBars;
-	}
+    }
 }
