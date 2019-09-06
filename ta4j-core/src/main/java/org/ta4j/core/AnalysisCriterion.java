e5d11b4855a68668d0bad39d6f5834595b625958
Thiago Moreira <tmoreira2020@gmail.com>
2019-09-06 14:19:57 -0300

    Format source

--- ta4j-core/src/main/java/org/ta4j/core/AnalysisCriterion.java
+++ ta4j-core/src/main/java/org/ta4j/core/AnalysisCriterion.java
@@ -39,56 +39,51 @@ import java.util.List;
 public interface AnalysisCriterion {
 
     /**
-     * @param series
-     *            a time series, not null
-     * @param trade
-     *            a trade, not null
+     * @param series a time series, not null
+     * @param trade  a trade, not null
      * @return the criterion value for the trade
      */
     Num calculate(TimeSeries series, Trade trade);
 
     /**
-     * @param series
-     *            a time series, not null
-     * @param tradingRecord
-     *            a trading record, not null
+     * @param series        a time series, not null
+     * @param tradingRecord a trading record, not null
      * @return the criterion value for the trades
      */
     Num calculate(TimeSeries series, TradingRecord tradingRecord);
 
     /**
-     * @param manager
-     *            the time series manager
-     * @param strategies
-     *            a list of strategies
-     * @return the best strategy (among the provided ones) according to the criterion
+     * @param manager    the time series manager
+     * @param strategies a list of strategies
+     * @return the best strategy (among the provided ones) according to the
+     *         criterion
      */
     default Strategy chooseBest(TimeSeriesManager manager, List<Strategy> strategies) {
 
-        Strategy bestStrategy = strategies.get(0);
-        Num bestCriterionValue = calculate(manager.getTimeSeries(), manager.run(bestStrategy));
+	Strategy bestStrategy = strategies.get(0);
+	Num bestCriterionValue = calculate(manager.getTimeSeries(), manager.run(bestStrategy));
 
-        for (int i = 1; i < strategies.size(); i++) {
-            Strategy currentStrategy = strategies.get(i);
-            Num currentCriterionValue = calculate(manager.getTimeSeries(), manager.run(currentStrategy));
+	for (int i = 1; i < strategies.size(); i++) {
+	    Strategy currentStrategy = strategies.get(i);
+	    Num currentCriterionValue = calculate(manager.getTimeSeries(), manager.run(currentStrategy));
 
-            if (betterThan(currentCriterionValue, bestCriterionValue)) {
-                bestStrategy = currentStrategy;
-                bestCriterionValue = currentCriterionValue;
-            }
-        }
+	    if (betterThan(currentCriterionValue, bestCriterionValue)) {
+		bestStrategy = currentStrategy;
+		bestCriterionValue = currentCriterionValue;
+	    }
+	}
 
-        return bestStrategy;
+	return bestStrategy;
     }
 
     /**
-     * @param criterionValue1
-     *            the first value
+     * @param criterionValue1 the first value
      * @param criterionValue2
      * 
      * 
-     *            the second value
-     * @return true if the first value is better than (according to the criterion) the second one, false otherwise
+     *                        the second value
+     * @return true if the first value is better than (according to the criterion)
+     *         the second one, false otherwise
      */
     boolean betterThan(Num criterionValue1, Num criterionValue2);
 }
