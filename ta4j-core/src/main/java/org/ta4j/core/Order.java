e8e1229ff54283a09f8b0e05addd6459cface5c4
nimo23 <enegy@hotmail.com>
2019-09-15 18:32:20 +0200

    Update Order.java
    changed equals()-method.

--- ta4j-core/src/main/java/org/ta4j/core/Order.java
+++ ta4j-core/src/main/java/org/ta4j/core/Order.java
@@ -265,6 +265,7 @@ public class Order implements Serializable {
 
     @Override
 	public boolean equals(Object obj) {
+		if (this == obj) return true;
 		if (obj == null || getClass() != obj.getClass()) return false;
 		final Order other = (Order) obj;
 		return Objects.equals(type, other.type) 
