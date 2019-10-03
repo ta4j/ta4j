1cd3a4975393ae8baac3a7e39bc107094befceb2
nimo23 <enegy@hotmail.com>
2019-10-03 23:09:01 +0200

    Update DoubleNumTest.java
    added testZeroEquals()

--- ta4j-core/src/test/java/org/ta4j/core/num/DoubleNumTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/num/DoubleNumTest.java
@@ -32,9 +32,16 @@ public class DoubleNumTest {
     @Test
     public void testEqualsDoubleNumWithPrecisionNum() {
         final PrecisionNum precisionNum = PrecisionNum.valueOf(3.0);
-
         final DoubleNum doubleNum = DoubleNum.valueOf(3.0);
-
+        
         assertFalse(doubleNum.equals(precisionNum));
+    }
+    
+    @Test
+    public void testZeroEquals() {
+        final Num num1 = DoubleNum.valueOf(-0.0);
+        final Num num2 = DoubleNum.valueOf(0.0);
+
+        assertTrue(num2.isEqual(num1));
     }
 }
