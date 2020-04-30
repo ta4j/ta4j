b6842812d322edc8f57fafa80412d9056e3e5276
nimo23 <enegy@hotmail.com>
2020-04-30 18:18:12 +0200

    Update AnalysisCriterion.java

--- ta4j-core/src/main/java/org/ta4j/core/AnalysisCriterion.java
+++ ta4j-core/src/main/java/org/ta4j/core/AnalysisCriterion.java
@@ -23,9 +23,12 @@
  */
 package org.ta4j.core;
 
-import org.ta4j.core.num.Num;
-
 import java.util.List;
+
+import org.ta4j.core.Order.OrderType;
+import org.ta4j.core.backtest.BacktestExecutor;
+import org.ta4j.core.backtest.BacktestResult;
+import org.ta4j.core.num.Num;
 
 /**
  * An analysis criterion.
@@ -38,52 +41,70 @@ import java.util.List;
  */
 public interface AnalysisCriterion {
 
-    /**
-     * @param series a bar series, not null
-     * @param trade  a trade, not null
-     * @return the criterion value for the trade
-     */
-    Num calculate(BarSeries series, Trade trade);
+	/**
+	 * @param series a bar series, not null
+	 * @param trade  a trade, not null
+	 * @return the criterion value for the trade
+	 */
+	Num calculate(BarSeries series, Trade trade);
 
-    /**
-     * @param series        a bar series, not null
-     * @param tradingRecord a trading record, not null
-     * @return the criterion value for the trades
-     */
-    Num calculate(BarSeries series, TradingRecord tradingRecord);
+	/**
+	 * @param series        a bar series, not null
+	 * @param tradingRecord a trading record, not null
+	 * @return the criterion value for the trades
+	 */
+	Num calculate(BarSeries series, TradingRecord tradingRecord);
 
-    /**
-     * @param manager    the bar series manager
-     * @param strategies a list of strategies
-     * @return the best strategy (among the provided ones) according to the
-     *         criterion
-     */
-    default Strategy chooseBest(BarSeriesManager manager, List<Strategy> strategies) {
+	/**
+	 * @param manager    the bar series manager
+	 * @param strategies a list of strategies
+	 * @return the best strategy (among the provided ones) according to the
+	 *         criterion
+	 */
+	default Strategy chooseBest(BarSeriesManager manager, List<Strategy> strategies) {
+		return new BacktestExecutor(this).chooseBest(manager, strategies).getStrategy();
+	}
 
-        Strategy bestStrategy = strategies.get(0);
-        Num bestCriterionValue = calculate(manager.getBarSeries(), manager.run(bestStrategy));
+	/**
+	 * @param manager    the bar series manager
+	 * @param strategies a list of strategies
+	 * @return the top list of AnalysisResult with the best strategies (among the
+	 *         provided ones) according to the criterion
+	 */
+	default List<BacktestResult> chooseBest(BarSeriesManager manager, List<Strategy> strategies, int topNumbers) {
+		return new BacktestExecutor(this).chooseBest(manager, strategies, topNumbers);
+	}
+	
+	/**
+	 * @param manager     the bar series manager
+	 * @param strategies  a list of strategies
+	 * @param orderType   the {@link OrderType} used to open the trades
+	 * @param startIndex  the start index for the run (included)
+	 * @param finishIndex the finish index for the run (included)
+	 * @return the result with the best strategy (among the provided ones) according
+	 *         to the criterion
+	 */
+	default BacktestResult chooseBest(BarSeriesManager manager, List<Strategy> strategies, OrderType orderType, int startIndex, int finishIndex) {
+		return new BacktestExecutor(this).chooseBest(manager, strategies, orderType, startIndex, finishIndex);
+	}
+	
+	/**
+	 * @param manager                the bar series manager
+	 * @param strategies             a list of strategies
+	 * @param requiredCriterionValue the required criterion value needed tested
+	 *                               against the best available criterion value
+	 * @return the AnalysisResult with the best strategy (among the provided ones)
+	 *         according to the criterion
+	 */
+	default BacktestResult chooseBest(BarSeriesManager manager, List<Strategy> strategies, Num requiredCriterionValue) {
+		return new BacktestExecutor(this).chooseBest(manager, strategies, requiredCriterionValue);
+	}
 
-        for (int i = 1; i < strategies.size(); i++) {
-            Strategy currentStrategy = strategies.get(i);
-            Num currentCriterionValue = calculate(manager.getBarSeries(), manager.run(currentStrategy));
-
-            if (betterThan(currentCriterionValue, bestCriterionValue)) {
-                bestStrategy = currentStrategy;
-                bestCriterionValue = currentCriterionValue;
-            }
-        }
-
-        return bestStrategy;
-    }
-
-    /**
-     * @param criterionValue1 the first value
-     * @param criterionValue2
-     * 
-     * 
-     *                        the second value
-     * @return true if the first value is better than (according to the criterion)
-     *         the second one, false otherwise
-     */
-    boolean betterThan(Num criterionValue1, Num criterionValue2);
+	/**
+	 * @param criterionValue1 the first value
+	 * @param criterionValue2 the second value
+	 * @return true if the first value is better than (according to the criterion)
+	 *         the second one, false otherwise
+	 */
+	boolean betterThan(Num criterionValue1, Num criterionValue2);
 }
