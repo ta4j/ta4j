3aaee072c4cd5bdce17d83204be73cd93c38a577
nimo23 <enegy@hotmail.com>
2021-01-20 09:06:42 +0100

    Update and rename ta4j-core/src/test/java/org/ta4j/core/analysis/criteria/ProfitLossPercentagePerTimeCriterionTest.java to ta4j-core/src/test/java/org/ta4j/core/analysis/criteria/pnl/ProfitLossPercentagePerTimeCriterionTest.java

--- ta4j-core/src/test/java/org/ta4j/core/analysis/criteria/pnl/ProfitLossPercentagePerTimeCriterionTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/analysis/criteria/pnl/ProfitLossPercentagePerTimeCriterionTest.java
@@ -21,7 +21,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package org.ta4j.core.analysis.criteria;
+package org.ta4j.core.analysis.criteria.pnl;
 
 import org.junit.Test;
 import org.ta4j.core.AnalysisCriterion;
