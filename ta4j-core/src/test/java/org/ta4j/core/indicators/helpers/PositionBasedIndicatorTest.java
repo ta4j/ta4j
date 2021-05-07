953e41262989c1f76b5dac55f195b71ab8f7ade4
Marc Dahlem <m_dahlem@cs.uni-kl.de>
2021-05-07 14:36:06 +0200

    Code formatter

--- ta4j-core/src/test/java/org/ta4j/core/indicators/helpers/PositionBasedIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/helpers/PositionBasedIndicatorTest.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 Marc de Verdelhan, 2017-2021 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package org.ta4j.core.indicators.helpers;
 
 import org.junit.Before;
@@ -18,71 +41,71 @@ import static org.ta4j.core.num.NaN.NaN;
 
 public class PositionBasedIndicatorTest extends AbstractIndicatorTest<Indicator<Num>, Num> {
 
-  private BarSeries series;
-  private BaseTradingRecord tradingRecord;
-  private TestPositionIndicator positionIndicator;
+    private BarSeries series;
+    private BaseTradingRecord tradingRecord;
+    private TestPositionIndicator positionIndicator;
 
-  public PositionBasedIndicatorTest(Function<Number, Num> numFunction) {
-    super(numFunction);
-  }
-
-  @Before
-  public void setUp() {
-    series = new MockBarSeries(numFunction);
-    tradingRecord = new BaseTradingRecord(PositionBasedIndicatorTest.class.getSimpleName());
-    positionIndicator = new TestPositionIndicator(series, tradingRecord);
-  }
-
-  @Test
-  public void indicatorReturnNanIfNoTradeAvailable() {
-    for (int index = series.getBeginIndex(); index <= series.getEndIndex(); index++) {
-      assertEquals(NaN, positionIndicator.getValue(index));
-      assertNull(positionIndicator.lastCalledEntryPosition);
-      assertNull(positionIndicator.lastCalledExitPosition);
-      assertEquals(-1, positionIndicator.lastCalledExitIndex);
-      assertEquals(-1, positionIndicator.lastCalledEntryIndex);
-      positionIndicator.reset();
-    }
-  }
-
-  private class TestPositionIndicator extends PositionBasedIndicator {
-    int lastCalledEntryIndex;
-    Position lastCalledEntryPosition;
-    Num lastReturnedEntryNumber;
-
-    int lastCalledExitIndex;
-    Position lastCalledExitPosition;
-    Num lastReturnedExitNumber;
-
-    public TestPositionIndicator(BarSeries series, BaseTradingRecord tradingRecord) {
-      super(series, tradingRecord);
-      reset();
+    public PositionBasedIndicatorTest(Function<Number, Num> numFunction) {
+        super(numFunction);
     }
 
-    @Override
-    Num calculateLastPositionWasEntry(Position entryPosition, int index) {
-      lastCalledEntryPosition = entryPosition;
-      lastCalledEntryIndex = index;
-      lastReturnedEntryNumber = getBarSeries().numOf(Math.random());
-      return lastReturnedEntryNumber;
+    @Before
+    public void setUp() {
+        series = new MockBarSeries(numFunction);
+        tradingRecord = new BaseTradingRecord(PositionBasedIndicatorTest.class.getSimpleName());
+        positionIndicator = new TestPositionIndicator(series, tradingRecord);
     }
 
-    @Override
-    Num calculateLastPositionWasExit(Position exitPosition, int index) {
-      lastCalledExitPosition = exitPosition;
-      lastCalledExitIndex = index;
-      lastReturnedExitNumber = getBarSeries().numOf(Math.random());
-      return lastReturnedExitNumber;
+    @Test
+    public void indicatorReturnNanIfNoTradeAvailable() {
+        for (int index = series.getBeginIndex(); index <= series.getEndIndex(); index++) {
+            assertEquals(NaN, positionIndicator.getValue(index));
+            assertNull(positionIndicator.lastCalledEntryPosition);
+            assertNull(positionIndicator.lastCalledExitPosition);
+            assertEquals(-1, positionIndicator.lastCalledExitIndex);
+            assertEquals(-1, positionIndicator.lastCalledEntryIndex);
+            positionIndicator.reset();
+        }
     }
 
-    void reset() {
-      lastCalledEntryIndex = -1;
-      lastCalledEntryPosition = null;
-      lastReturnedEntryNumber = null;
+    private class TestPositionIndicator extends PositionBasedIndicator {
+        int lastCalledEntryIndex;
+        Position lastCalledEntryPosition;
+        Num lastReturnedEntryNumber;
 
-      lastCalledExitIndex = -1;
-      lastCalledExitPosition = null;
-      lastReturnedExitNumber = null;
+        int lastCalledExitIndex;
+        Position lastCalledExitPosition;
+        Num lastReturnedExitNumber;
+
+        public TestPositionIndicator(BarSeries series, BaseTradingRecord tradingRecord) {
+            super(series, tradingRecord);
+            reset();
+        }
+
+        @Override
+        Num calculateLastPositionWasEntry(Position entryPosition, int index) {
+            lastCalledEntryPosition = entryPosition;
+            lastCalledEntryIndex = index;
+            lastReturnedEntryNumber = getBarSeries().numOf(Math.random());
+            return lastReturnedEntryNumber;
+        }
+
+        @Override
+        Num calculateLastPositionWasExit(Position exitPosition, int index) {
+            lastCalledExitPosition = exitPosition;
+            lastCalledExitIndex = index;
+            lastReturnedExitNumber = getBarSeries().numOf(Math.random());
+            return lastReturnedExitNumber;
+        }
+
+        void reset() {
+            lastCalledEntryIndex = -1;
+            lastCalledEntryPosition = null;
+            lastReturnedEntryNumber = null;
+
+            lastCalledExitIndex = -1;
+            lastCalledExitPosition = null;
+            lastReturnedExitNumber = null;
+        }
     }
-  }
 }
