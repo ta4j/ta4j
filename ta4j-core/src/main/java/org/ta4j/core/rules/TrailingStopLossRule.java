57f99c736e47e4057502de2bd28f1a92d7143e5a
Simon W <simonjwimmer@icloud.com>
2021-05-28 19:37:39 +0200

    Feature/clean trailing stop loss rule (#781)
    * Clean source code of TrailingStopLossRule class
    
    * Clean source code of TrailingStopLossRule class

--- ta4j-core/src/main/java/org/ta4j/core/rules/TrailingStopLossRule.java
+++ ta4j-core/src/main/java/org/ta4j/core/rules/TrailingStopLossRule.java
@@ -21,28 +21,6 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-/*
-  The MIT License (MIT)
-
-  Copyright (c) 2014-2018 Marc de Verdelhan & respective authors (see AUTHORS)
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy of
-  this software and associated documentation files (the "Software"), to deal in
-  the Software without restriction, including without limitation the rights to
-  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-  the Software, and to permit persons to whom the Software is furnished to do so,
-  subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 package org.ta4j.core.rules;
 
 import org.ta4j.core.Indicator;
@@ -63,28 +41,27 @@ public class TrailingStopLossRule extends AbstractRule {
      * The price indicator
      */
     private final Indicator<Num> priceIndicator;
-    /**
-     * the current price extremum
-     */
-    private Num currentExtremum = null;
-    /**
-     * the current stop loss price activation
-     */
-    private Num currentStopLossLimitActivation = null;
+
     /** The barCount */
-    private int barCount;
+    private final int barCount;
+
     /** the loss-distance as percentage */
     private final Num lossPercentage;
 
     /**
+     * the current stop loss price activation
+     */
+    private Num currentStopLossLimitActivation;
+
+    /**
      * Constructor.
      * 
-     * @param closePrice     the close price indicator
+     * @param indicator      the (close price) indicator
      * @param lossPercentage the loss percentage
      * @param barCount       number of bars to look back for the calculation
      */
-    public TrailingStopLossRule(Indicator<Num> priceIndicator, Num lossPercentage, int barCount) {
-        this.priceIndicator = priceIndicator;
+    public TrailingStopLossRule(Indicator<Num> indicator, Num lossPercentage, int barCount) {
+        this.priceIndicator = indicator;
         this.barCount = barCount;
         this.lossPercentage = lossPercentage;
     }
@@ -92,11 +69,11 @@ public class TrailingStopLossRule extends AbstractRule {
     /**
      * Constructor.
      * 
-     * @param closePrice     the close price indicator
+     * @param indicator      the (close price) indicator
      * @param lossPercentage the loss percentage
      */
-    public TrailingStopLossRule(Indicator<Num> priceIndicator, Num lossPercentage) {
-        this(priceIndicator, lossPercentage, Integer.MAX_VALUE);
+    public TrailingStopLossRule(Indicator<Num> indicator, Num lossPercentage) {
+        this(indicator, lossPercentage, Integer.MAX_VALUE);
     }
 
     @Override
