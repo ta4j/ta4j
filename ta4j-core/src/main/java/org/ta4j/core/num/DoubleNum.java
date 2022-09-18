978f9806507ad6b1a4d8a29e28463399d2f88f03
Jim Northrup <jnorthrup@users.noreply.github.com>
2022-09-18 21:34:26 +0800

    license bug in devops script

--- ta4j-core/src/main/java/org/ta4j/core/num/DoubleNum.java
+++ ta4j-core/src/main/java/org/ta4j/core/num/DoubleNum.java
@@ -1,7 +1,8 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2017-2022 Ta4j Organization & respective * authors (see AUTHORS)
+ * Copyright (c) 2017-2022 Ta4j Organization & respective
+ * authors (see AUTHORS)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
