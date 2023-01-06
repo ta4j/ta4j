ffd7d71a5f81626a4641a20b2f966bb688778ae7
David <cookiefactory@gmail.com>
2023-01-06 14:20:12 -0500

    formatting for touched files

--- ta4j-core/src/test/java/org/ta4j/core/criteria/ExpectancyCriterionTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/criteria/ExpectancyCriterionTest.java
@@ -1,25 +1,16 @@
 /**
- * The MIT License (MIT)
+ * This copy of Woodstox XML processor is licensed under the
+ * Apache (Software) License, version 2.0 ("the License").
+ * See the License for details about distribution rights, and the
+ * specific rights regarding derivate works.
  *
- * Copyright (c) 2017-2022 Ta4j Organization & respective
- * authors (see AUTHORS)
+ * You may obtain a copy of the License at:
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * http://www.apache.org/licenses/
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * A copy is also included in the downloadable source code package
+ * containing Woodstox, in file "ASL2.0", under the same directory
+ * as this file.
  */
 package org.ta4j.core.criteria;
 
@@ -62,7 +53,7 @@ public class ExpectancyCriterionTest extends AbstractCriterionTest {
         AnalysisCriterion avgLoss = getCriterion();
         assertNumEquals(0.25, avgLoss.calculate(series, tradingRecord));
     }
-    
+
     @Test
     public void calculateOnlyWithLossPositions() {
         MockBarSeries series = new MockBarSeries(numFunction, 100, 95, 80, 70, 60, 50);
