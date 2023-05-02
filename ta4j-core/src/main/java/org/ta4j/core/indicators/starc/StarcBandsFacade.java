a1a8338604e7ede92b6e26b028bc633d41f7fa8e
David <cookiefactory@gmail.com>
2023-05-02 11:57:35 -0400

    adding StarC bands

---
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/starc/StarcBandsFacade.java
@@ -0,0 +1,63 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2017-2023 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.starc;
+
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.Indicator;
+import org.ta4j.core.indicators.ATRIndicator;
+import org.ta4j.core.indicators.SMAIndicator;
+import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
+import org.ta4j.core.indicators.helpers.TransformIndicator;
+import org.ta4j.core.indicators.numeric.NumericIndicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * A facade to create the 3 STARC Bands indicators. A simple moving
+ * average of close price is used as the middle channel.
+ */
+public class StarcBandsFacade {
+
+    private final StarcBandsMiddleIndicator middle;
+    private final StarcBandsUpperIndicator upper;
+    private final StarcBandsLowerIndicator lower;
+
+    public StarcBandsFacade(BarSeries series, int smaBarCount, int atrBarCount, Number atrMultiplier) {
+        this.middle = new StarcBandsMiddleIndicator(series, smaBarCount);
+        this.upper = new StarcBandsUpperIndicator(this.middle, atrBarCount, atrMultiplier);
+        this.lower = new StarcBandsLowerIndicator(this.middle, atrBarCount, atrMultiplier);
+    }
+
+    public StarcBandsMiddleIndicator middle() {
+        return middle;
+    }
+
+    public StarcBandsUpperIndicator upper() {
+        return upper;
+    }
+
+    public StarcBandsLowerIndicator lower() {
+        return lower;
+    }
+
+}
