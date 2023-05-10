39f4fe98d0e3fb8e041b59bc89cb90b74e021598
David Pang <davidzhe@gmail.com>
2023-05-10 10:33:43 -0400

    added formatting

--- ta4j-core/src/test/java/org/ta4j/core/indicators/starc/StarcBandsUpperIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/starc/StarcBandsUpperIndicatorTest.java
@@ -1,19 +1,19 @@
 /**
  * The MIT License (MIT)
- * <p>
+ *
  * Copyright (c) 2017-2023 Ta4j Organization & respective
  * authors (see AUTHORS)
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -59,7 +59,8 @@ public class StarcBandsUpperIndicatorTest extends AbstractIndicatorTest<Indicato
         bars.add(new MockBar(11, 11, 11, 11, 10, numFunction));
         BarSeries series = new MockBarSeries(bars);
 
-        TimeSegmentedVolumeIndicator tsv = new TimeSegmentedVolumeIndicator(series, 5);
+        StarcBandsMiddleIndicator middle = new StarcBandsMiddleIndicator(series, 5);
+        StarcBandsUpperIndicator tsv = new StarcBandsUpperIndicator(middle, 5, 2);
 
         assertTrue(tsv.getValue(0).isNaN());
         assertTrue(tsv.getValue(1).isNaN());
