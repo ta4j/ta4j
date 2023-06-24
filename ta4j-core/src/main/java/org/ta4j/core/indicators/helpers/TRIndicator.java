99622bba62bd2d34ecb8f542af382c953af9b49f
nimo23 <enegy@hotmail.com>
2023-06-24 13:43:47 +0200

    improve TRIndicator#calculate

--- ta4j-core/src/main/java/org/ta4j/core/indicators/helpers/TRIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/helpers/TRIndicator.java
@@ -23,27 +23,45 @@
  */
 package org.ta4j.core.indicators.helpers;
 
+import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.indicators.CachedIndicator;
 import org.ta4j.core.num.Num;
 
 /**
  * True range indicator.
+ * 
+ * <pre>
+ * TrueRange = MAX(high - low, high - previousClose, previousClose - low)
+ * </pre>
  */
 public class TRIndicator extends CachedIndicator<Num> {
 
+    /**
+     * Constructor.
+     * 
+     * @param series the bar series
+     */
     public TRIndicator(BarSeries series) {
         super(series);
     }
 
     @Override
     protected Num calculate(int index) {
-        Num ts = getBarSeries().getBar(index).getHighPrice().minus(getBarSeries().getBar(index).getLowPrice());
-        Num ys = index == 0 ? zero()
-                : getBarSeries().getBar(index).getHighPrice().minus(getBarSeries().getBar(index - 1).getClosePrice());
-        Num yst = index == 0 ? zero()
-                : getBarSeries().getBar(index - 1).getClosePrice().minus(getBarSeries().getBar(index).getLowPrice());
-        return ts.abs().max(ys.abs()).max(yst.abs());
+        Bar bar = getBarSeries().getBar(index);
+        Num high = bar.getHighPrice();
+        Num low = bar.getLowPrice();
+        Num hl = high.minus(low);
+
+        if (index == 0) {
+            return hl.abs();
+        }
+
+        Num previousClose = getBarSeries().getBar(index - 1).getClosePrice();
+        Num hc = high.minus(previousClose);
+        Num cl = previousClose.minus(low);
+        return hl.abs().max(hc.abs()).max(cl.abs());
+
     }
 
     @Override
