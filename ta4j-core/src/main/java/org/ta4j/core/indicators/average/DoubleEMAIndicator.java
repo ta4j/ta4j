652b72cfcc713ceaa1e9813787f452860619fd5f
Lukáš Kvídera <kvideral@qwsome.eu>
2024-04-24 08:57:06 +0200

    Enabled DoubleEMAIndicator

--- ta4j-core/src/main/java/org/ta4j/core/indicators/average/DoubleEMAIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/average/DoubleEMAIndicator.java
@@ -1,78 +1,96 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core.indicators.average;
-//
-//import org.ta4j.core.indicators.AbstractIndicator;
-//import org.ta4j.core.indicators.Indicator;
-//import org.ta4j.core.num.Num;
-//
-///**
-// * Double exponential moving average indicator.
-// *
-// * @see <a href=
-// *      "https://en.wikipedia.org/wiki/Double_exponential_moving_average">
-// *      https://en.wikipedia.org/wiki/Double_exponential_moving_average</a>
-// */
-//public class DoubleEMAIndicator extends AbstractIndicator<Num> {
-//
-//    private final Num two;
-//    private final int barCount;
-//    private final EMAIndicator ema;
-//    private final EMAIndicator emaEma;
-//
-//    /**
-//     * Constructor.
-//     *
-//     * @param indicator the indicator
-//     * @param barCount  the time frame
-//     */
-//    public DoubleEMAIndicator(Indicator<Num> indicator, int barCount) {
-//        super(indicator.getBarSeries());
-//        this.two = getBarSeries().numFactory().two();
-//        this.barCount = barCount;
-//        this.ema = new EMAIndicator(indicator, barCount);
-//        this.emaEma = new EMAIndicator(ema, barCount);
-//    }
-//
-//    @Override
-//    public Num getValue() {
-//        return calculate();
-//    }
-//
-//
-//    protected Num calculate() {
-//        return ema.getValue().multipliedBy(two).minus(emaEma.getValue());
-//    }
-//
-//
-//    @Override
-//    public int getUnstableBars() {
-//        return barCount;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return getClass().getSimpleName() + " barCount: " + barCount;
-//    }
-//}
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.average;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.ta4j.core.indicators.AbstractIndicator;
+import org.ta4j.core.indicators.Indicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * Double exponential moving average indicator.
+ *
+ * @see <a href=
+ *     "https://en.wikipedia.org/wiki/Double_exponential_moving_average">
+ *     https://en.wikipedia.org/wiki/Double_exponential_moving_average</a>
+ */
+public class DoubleEMAIndicator extends AbstractIndicator<Num> {
+
+  private final int barCount;
+  private final EMAIndicator ema;
+  private final EMAIndicator emaEma;
+  private ZonedDateTime currentTick = ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.systemDefault());
+  private Num value;
+
+
+  /**
+   * Constructor.
+   *
+   * @param indicator the indicator
+   * @param barCount the time frame
+   */
+  public DoubleEMAIndicator(final Indicator<Num> indicator, final int barCount) {
+    super(indicator.getBarSeries());
+    this.barCount = barCount;
+    this.ema = new EMAIndicator(indicator, barCount);
+    this.emaEma = new EMAIndicator(this.ema, barCount);
+  }
+
+
+  @Override
+  public Num getValue() {
+    return this.value;
+  }
+
+
+  @Override
+  public void refresh(final ZonedDateTime tick) {
+    if (tick.isAfter(this.currentTick)) {
+      this.ema.refresh(tick);
+      this.emaEma.refresh(tick);
+      this.value = calculate();
+      this.currentTick = tick;
+    }
+  }
+
+
+  @Override
+  public boolean isStable() {
+    return this.ema.isStable() && this.emaEma.isStable();
+  }
+
+
+  protected Num calculate() {
+    return this.ema.getValue().multipliedBy(getBarSeries().numFactory().two()).minus(this.emaEma.getValue());
+  }
+
+
+  @Override
+  public String toString() {
+    return getClass().getSimpleName() + " barCount: " + this.barCount;
+  }
+}
