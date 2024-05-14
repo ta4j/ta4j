aa788672ffd480521c546e7553de8b57c1cff11f
Lukáš Kvídera <kvideral@qwsome.eu>
2024-05-14 20:15:18 +0200

    Enabled StandardDeviationIndicator
    - FIXED tests based on fixed VarianceIndicator

--- ta4j-core/src/main/java/org/ta4j/core/indicators/statistics/StandardDeviationIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/statistics/StandardDeviationIndicator.java
@@ -1,60 +1,85 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core.indicators.statistics;
-//
-//import org.ta4j.core.indicators.Indicator;
-//import org.ta4j.core.num.Num;
-//
-///**
-// * Standard deviation indicator.
-// *
-// * @see <a href=
-// *      "http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:standard_deviation_volatility">
-// *      http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:standard_deviation_volatility</a>
-// */
-//public class StandardDeviationIndicator extends CachedIndicator<Num> {
-//
-//    private final VarianceIndicator variance;
-//
-//    /**
-//     * Constructor.
-//     *
-//     * @param indicator the indicator
-//     * @param barCount  the time frame
-//     */
-//    public StandardDeviationIndicator(Indicator<Num> indicator, int barCount) {
-//        super(indicator);
-//        this.variance = new VarianceIndicator(indicator, barCount);
-//    }
-//
-//    @Override
-//    protected Num calculate(int index) {
-//        return variance.getValue(index).sqrt();
-//    }
-//
-//    @Override
-//    public int getUnstableBars() {
-//        return 0;
-//    }
-//}
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.statistics;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.ta4j.core.indicators.AbstractIndicator;
+import org.ta4j.core.indicators.Indicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * Standard deviation indicator.
+ *
+ * @see <a href=
+ *     "http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:standard_deviation_volatility">
+ *     http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:standard_deviation_volatility</a>
+ */
+public class StandardDeviationIndicator extends AbstractIndicator<Num> {
+
+  private final VarianceIndicator variance;
+  private ZonedDateTime currentTick = ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.systemDefault());
+  private Num value;
+
+
+  /**
+   * Constructor.
+   *
+   * @param indicator the indicator
+   * @param barCount the time frame
+   */
+  public StandardDeviationIndicator(final Indicator<Num> indicator, final int barCount) {
+    super(indicator.getBarSeries());
+    this.variance = new VarianceIndicator(indicator, barCount);
+  }
+
+
+  protected Num calculate() {
+    return this.variance.getValue().sqrt();
+  }
+
+
+  @Override
+  public Num getValue() {
+    return this.value;
+  }
+
+
+  @Override
+  public void refresh(final ZonedDateTime tick) {
+    if (tick.isAfter(this.currentTick)) {
+      this.variance.refresh(tick);
+      this.value = calculate();
+      this.currentTick = tick;
+    }
+  }
+
+
+  @Override
+  public boolean isStable() {
+    return this.variance.isStable();
+  }
+}
--- ta4j-core/src/test/java/org/ta4j/core/indicators/statistics/StandardDeviationIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/statistics/StandardDeviationIndicatorTest.java
@@ -1,73 +1,77 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core.indicators.statistics;
-//
-//import static org.ta4j.core.TestUtils.assertNumEquals;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.ta4j.core.BarSeries;
-//import org.ta4j.core.indicators.Indicator;
-//import org.ta4j.core.indicators.AbstractIndicatorTest;
-//import org.ta4j.core.indicators.candles.price.ClosePriceIndicator;
-//import org.ta4j.core.mocks.MockBarSeriesBuilder;
-//import org.ta4j.core.num.Num;
-//import org.ta4j.core.num.NumFactory;
-//
-//public class StandardDeviationIndicatorTest extends AbstractIndicatorTest<Indicator<Num>, Num> {
-//    private BarSeries data;
-//
-//    public StandardDeviationIndicatorTest(NumFactory numFunction) {
-//        super(numFunction);
-//    }
-//
-//    @Before
-//    public void setUp() {
-//        data = new MockBarSeriesBuilder().withNumFactory(numFactory).withData(1, 2, 3, 4, 3, 4, 5, 4, 3, 0, 9).build();
-//    }
-//
-//    @Test
-//    public void standardDeviationUsingBarCount4UsingClosePrice() {
-//        var sdv = new StandardDeviationIndicator(new ClosePriceIndicator(data), 4);
-//
-//        assertNumEquals(0, sdv.getValue(0));
-//        assertNumEquals(Math.sqrt(0.25), sdv.getValue(1));
-//        assertNumEquals(Math.sqrt(2.0 / 3), sdv.getValue(2));
-//        assertNumEquals(Math.sqrt(1.25), sdv.getValue(3));
-//        assertNumEquals(Math.sqrt(0.5), sdv.getValue(4));
-//        assertNumEquals(Math.sqrt(0.25), sdv.getValue(5));
-//        assertNumEquals(Math.sqrt(0.5), sdv.getValue(6));
-//        assertNumEquals(Math.sqrt(0.5), sdv.getValue(7));
-//        assertNumEquals(Math.sqrt(0.5), sdv.getValue(8));
-//        assertNumEquals(Math.sqrt(3.5), sdv.getValue(9));
-//        assertNumEquals(Math.sqrt(10.5), sdv.getValue(10));
-//    }
-//
-//    @Test
-//    public void standardDeviationShouldBeZeroWhenBarCountIs1() {
-//        var sdv = new StandardDeviationIndicator(new ClosePriceIndicator(data), 1);
-//        assertNumEquals(0, sdv.getValue(3));
-//        assertNumEquals(0, sdv.getValue(8));
-//    }
-//}
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.statistics;
+
+import static org.ta4j.core.TestUtils.assertNext;
+import static org.ta4j.core.TestUtils.fastForward;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.ta4j.core.MockStrategy;
+import org.ta4j.core.backtest.BacktestBarSeries;
+import org.ta4j.core.indicators.AbstractIndicatorTest;
+import org.ta4j.core.indicators.Indicator;
+import org.ta4j.core.indicators.candles.price.ClosePriceIndicator;
+import org.ta4j.core.mocks.MockBarSeriesBuilder;
+import org.ta4j.core.num.Num;
+import org.ta4j.core.num.NumFactory;
+
+public class StandardDeviationIndicatorTest extends AbstractIndicatorTest<Indicator<Num>, Num> {
+  private BacktestBarSeries data;
+
+
+  public StandardDeviationIndicatorTest(final NumFactory numFunction) {
+    super(numFunction);
+  }
+
+
+  @Before
+  public void setUp() {
+    this.data =
+        new MockBarSeriesBuilder().withNumFactory(this.numFactory).withData(1, 2, 3, 4, 3, 4, 5, 4, 3, 0, 9).build();
+  }
+
+
+  @Test
+  public void standardDeviationUsingBarCount4UsingClosePrice() {
+    final var sdv = new StandardDeviationIndicator(new ClosePriceIndicator(this.data), 4);
+    this.data.replaceStrategy(new MockStrategy(sdv));
+
+    fastForward(this.data, 4);
+    assertNext(this.data, 1.291, sdv);
+    assertNext(this.data, 0.81649, sdv);
+    assertNext(this.data, 0.57735, sdv);
+    assertNext(this.data, 0.81649, sdv);
+    assertNext(this.data, 0.81649, sdv);
+    assertNext(this.data, 0.81649, sdv);
+    assertNext(this.data, 2.1602, sdv);
+    assertNext(this.data, 3.7416, sdv);
+  }
+
+
+  @Test(expected = IllegalArgumentException.class)
+  public void standardDeviationShouldBeZeroWhenBarCountIs1() {
+    new StandardDeviationIndicator(new ClosePriceIndicator(this.data), 1);
+  }
+}
