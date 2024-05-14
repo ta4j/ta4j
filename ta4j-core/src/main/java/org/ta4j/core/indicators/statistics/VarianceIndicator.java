f916824a07187dd652468e076d8013a35caf4422
Lukáš Kvídera <kvideral@qwsome.eu>
2024-05-14 20:00:04 +0200

    Enabled VarianceIndicator
    - FIXED calculation that is comparable to VAR() in LibreOffice now

--- ta4j-core/src/main/java/org/ta4j/core/indicators/statistics/VarianceIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/statistics/VarianceIndicator.java
@@ -1,118 +1,117 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core.indicators.statistics;
-//
-//import org.ta4j.core.indicators.AbstractIndicator;
-//import org.ta4j.core.indicators.Indicator;
-//import org.ta4j.core.indicators.average.SMAIndicator;
-//import org.ta4j.core.indicators.helpers.RunningTotalIndicator;
-//import org.ta4j.core.num.Num;
-//
-///**
-// * Variance indicator.
-// */
-//public class VarianceIndicator extends AbstractIndicator<Num> {
-//
-//    private final int barCount;
-//    private final RunningTotalIndicator variance;
-//
-//    /**
-//     * Constructor.
-//     *
-//     * @param indicator the indicator
-//     * @param barCount  the time frame
-//     */
-//    public VarianceIndicator(Indicator<Num> indicator, int barCount) {
-//        super(indicator.getBarSeries());
-//        this.barCount = barCount;
-//        this.variance = new RunningTotalIndicator(new PowIndicator(indicator,barCount),barCount);
-//    }
-//
-//    protected Num calculate() {
-//        final var numFactory = getBarSeries().numFactory();
-//        return variance.getValue().dividedBy(numFactory.numOf(barCount));
-//    }
-//
-//
-//    @Override
-//    public Num getValue() {
-//        return calculate();
-//    }
-//
-//
-//    @Override
-//    public int getUnstableBars() {
-//        return barCount;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return getClass().getSimpleName() + " barCount: " + barCount;
-//    }
-//
-//
-//    public static class PowIndicator extends AbstractIndicator<Num> {
-//        private final Indicator<Num> indicator;
-//        private final SMAIndicator sma;
-//        private final int barCount;
-//
-//
-//        /**
-//         * Constructor.
-//         *
-//         * @param indicator the bar indicator
-//         */
-//        public PowIndicator(Indicator<Num> indicator, int barCount) {
-//            super(indicator.getBarSeries());
-//            this.barCount = barCount;
-//            this.indicator = indicator;
-//            this.sma = new SMAIndicator(indicator, barCount);
-//        }
-//
-//
-//        public Num getValue() {
-//            return calculate();
-//        }
-//
-//        /**
-//         * Calculates the difference between indicator values of the current bar and the
-//         * previous bar.
-//         *
-//         * @return the difference between the close prices
-//         */
-//        protected Num calculate() {
-//            return pow();
-//        }
-//
-//        private Num pow() {
-//            return indicator.getValue().minus(sma.getValue()).pow(2);
-//        }
-//
-//
-//        @Override
-//        public int getUnstableBars() {
-//            return barCount;
-//        }
-//    }
-//}
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.statistics;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.LinkedList;
+
+import org.ta4j.core.indicators.AbstractIndicator;
+import org.ta4j.core.indicators.Indicator;
+import org.ta4j.core.indicators.average.SMAIndicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * Variance indicator.
+ */
+public class VarianceIndicator extends AbstractIndicator<Num> {
+
+  private final int barCount;
+  private final Num divisor;
+  private final SMAIndicator mean;
+  // TODO circular buffer
+  private final LinkedList<Num> values = new LinkedList<>();
+  private final Indicator<Num> indicator;
+  private ZonedDateTime currentTick = ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.systemDefault());
+  private Num value;
+
+
+  /**
+   * Constructor.
+   *
+   * @param indicator the indicator
+   * @param barCount the time frame
+   */
+  public VarianceIndicator(final Indicator<Num> indicator, final int barCount) {
+    super(indicator.getBarSeries());
+    this.barCount = barCount;
+    this.indicator = indicator;
+    this.divisor = this.indicator.getBarSeries().numFactory().numOf(this.barCount - 1);
+    this.mean = new SMAIndicator(indicator, barCount);
+
+    if (barCount <= 1) {
+      throw new IllegalArgumentException("barCount must be greater than 1");
+    }
+  }
+
+
+  protected Num calculate() {
+    this.values.addLast(this.indicator.getValue());
+
+    if (this.values.size() > this.barCount) {
+      this.values.removeFirst();
+    }
+
+    Num variance = getBarSeries().numFactory().zero();
+    // cannot use RunningTotalIndicator because mean is changing each tick
+    final Num average = this.mean.getValue();
+    for (final var val : this.values) {
+      final var diff = val.minus(average);
+      final Num pow = diff.multipliedBy(diff);
+      variance = variance.plus(pow);
+    }
+
+    variance = variance.dividedBy(this.divisor);
+    return variance;
+  }
+
+
+  @Override
+  public Num getValue() {
+    return this.value;
+  }
+
+
+  @Override
+  public void refresh(final ZonedDateTime tick) {
+    if (tick.isAfter(this.currentTick)) {
+      this.mean.refresh(tick);
+      this.value = calculate();
+      this.currentTick = tick;
+    }
+  }
+
+
+  @Override
+  public boolean isStable() {
+    return this.indicator.isStable() && this.mean.isStable();
+  }
+
+
+  @Override
+  public String toString() {
+    return getClass().getSimpleName() + " barCount: " + this.barCount;
+  }
+}
--- ta4j-core/src/test/java/org/ta4j/core/indicators/statistics/VarianceIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/statistics/VarianceIndicatorTest.java
@@ -1,91 +1,113 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core.indicators.statistics;
-//
-//import static org.ta4j.core.TestUtils.assertNumEquals;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.ta4j.core.BarSeries;
-//import org.ta4j.core.indicators.Indicator;
-//import org.ta4j.core.indicators.AbstractIndicatorTest;
-//import org.ta4j.core.indicators.candles.price.ClosePriceIndicator;
-//import org.ta4j.core.mocks.MockBarSeriesBuilder;
-//import org.ta4j.core.num.Num;
-//import org.ta4j.core.num.NumFactory;
-//
-//public class VarianceIndicatorTest extends AbstractIndicatorTest<Indicator<Num>, Num> {
-//    private BarSeries data;
-//
-//    public VarianceIndicatorTest(NumFactory numFactory) {
-//        super(numFactory);
-//    }
-//
-//    @Before
-//    public void setUp() {
-//        data = new MockBarSeriesBuilder().withNumFactory(numFactory).withData(1, 2, 3, 4, 3, 4, 5, 4, 3, 0, 9).build();
-//    }
-//
-//    @Test
-//    public void varianceUsingBarCount4UsingClosePrice() {
-//        var var = new VarianceIndicator(new ClosePriceIndicator(data), 4);
-//
-//        assertNumEquals(0, var.getValue(0));
-//        assertNumEquals(0.25, var.getValue(1));
-//        assertNumEquals(2.0 / 3, var.getValue(2));
-//        assertNumEquals(1.25, var.getValue(3));
-//        assertNumEquals(0.5, var.getValue(4));
-//        assertNumEquals(0.25, var.getValue(5));
-//        assertNumEquals(0.5, var.getValue(6));
-//        assertNumEquals(0.5, var.getValue(7));
-//        assertNumEquals(0.5, var.getValue(8));
-//        assertNumEquals(3.5, var.getValue(9));
-//        assertNumEquals(10.5, var.getValue(10));
-//    }
-//
-//    @Test
-//    public void firstValueShouldBeZero() {
-//        var var = new VarianceIndicator(new ClosePriceIndicator(data), 4);
-//        assertNumEquals(0, var.getValue(0));
-//    }
-//
-//    @Test
-//    public void varianceShouldBeZeroWhenBarCountIs1() {
-//        var var = new VarianceIndicator(new ClosePriceIndicator(data), 1);
-//        assertNumEquals(0, var.getValue(3));
-//        assertNumEquals(0, var.getValue(8));
-//    }
-//
-//    @Test
-//    public void varianceUsingBarCount2UsingClosePrice() {
-//        var var = new VarianceIndicator(new ClosePriceIndicator(data), 2);
-//
-//        assertNumEquals(0, var.getValue(0));
-//        assertNumEquals(0.25, var.getValue(1));
-//        assertNumEquals(0.25, var.getValue(2));
-//        assertNumEquals(0.25, var.getValue(3));
-//        assertNumEquals(2.25, var.getValue(9));
-//        assertNumEquals(20.25, var.getValue(10));
-//    }
-//}
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.statistics;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.ta4j.core.TestUtils.assertNext;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.ta4j.core.MockStrategy;
+import org.ta4j.core.backtest.BacktestBarSeries;
+import org.ta4j.core.indicators.AbstractIndicatorTest;
+import org.ta4j.core.indicators.Indicator;
+import org.ta4j.core.indicators.candles.price.ClosePriceIndicator;
+import org.ta4j.core.mocks.MockBarSeriesBuilder;
+import org.ta4j.core.num.Num;
+import org.ta4j.core.num.NumFactory;
+
+public class VarianceIndicatorTest extends AbstractIndicatorTest<Indicator<Num>, Num> {
+  private static final Logger LOG = LoggerFactory.getLogger(VarianceIndicatorTest.class);
+  private BacktestBarSeries data;
+
+
+  public VarianceIndicatorTest(final NumFactory numFactory) {
+    super(numFactory);
+  }
+
+
+  @Before
+  public void setUp() {
+    this.data =
+        new MockBarSeriesBuilder().withNumFactory(this.numFactory).withData(1, 2, 3, 4, 3, 4, 5, 4, 3, 0, 9).build();
+  }
+
+
+  @Test
+  public void varianceUsingBarCount4UsingClosePrice() {
+    final var var = new VarianceIndicator(new ClosePriceIndicator(this.data), 4);
+    this.data.replaceStrategy(new MockStrategy(var));
+
+    // unstable values may produce garbage, this is why they are called unstable
+    assertNext(this.data, 0.1875, var);
+    assertFalse(var.isStable());
+
+    assertNext(this.data, 0.5416, var);
+    assertFalse(var.isStable());
+
+    assertNext(this.data, 0.9166, var);
+    assertFalse(var.isStable());
+
+    // stable date bellow
+    assertNext(this.data, 1.6667, var);
+    assertTrue(var.isStable());
+
+    assertNext(this.data, 0.6667, var);
+    assertTrue(var.isStable());
+
+    assertNext(this.data, 0.3333, var);
+    assertNext(this.data, 0.6667, var);
+    assertNext(this.data, 0.6667, var);
+    assertNext(this.data, 0.6667, var);
+    assertNext(this.data, 4.6667, var);
+    assertNext(this.data, 14.000, var);
+  }
+
+
+  @Test(expected = IllegalArgumentException.class)
+  public void varianceShouldBeZeroWhenBarCountIs1() {
+     new VarianceIndicator(new ClosePriceIndicator(this.data), 1);
+  }
+
+
+  @Test
+  public void varianceUsingBarCount2UsingClosePrice() {
+    final var var = new VarianceIndicator(new ClosePriceIndicator(this.data), 2);
+    this.data.replaceStrategy(new MockStrategy(var));
+
+    assertNext(this.data, 0.25, var);
+    assertNext(this.data, 0.5, var);
+    assertNext(this.data, 0.5, var);
+    assertNext(this.data, 0.5, var);
+    assertNext(this.data, 0.5, var);
+    assertNext(this.data, 0.5, var);
+    assertNext(this.data, 0.5, var);
+    assertNext(this.data, 0.5, var);
+    assertNext(this.data, 0.5, var);
+    assertNext(this.data, 4.5, var);
+  }
+}
