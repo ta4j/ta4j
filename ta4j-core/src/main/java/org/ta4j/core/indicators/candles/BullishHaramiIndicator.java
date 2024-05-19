0b4de69413ea0fcf74bc39af90135e5c09276fc2
Lukáš Kvídera <kvideral@qwsome.eu>
2024-05-19 15:17:15 +0200

    Enabled BullishHaramiIndicator

--- ta4j-core/src/main/java/org/ta4j/core/indicators/candles/BullishHaramiIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/candles/BullishHaramiIndicator.java
@@ -1,70 +1,102 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core.indicators.candles;
-//
-//import org.ta4j.core.Bar;
-//import org.ta4j.core.BarSeries;
-//import org.ta4j.core.num.Num;
-//
-///**
-// * Bearish Harami pattern indicator.
-// *
-// * @see <a href="http://www.investopedia.com/terms/b/bullishharami.asp">
-// *      http://www.investopedia.com/terms/b/bullishharami.asp</a>
-// */
-//public class BullishHaramiIndicator extends CachedIndicator<Boolean> {
-//
-//    /**
-//     * Constructor.
-//     *
-//     * @param series the bar series
-//     */
-//    public BullishHaramiIndicator(BarSeries series) {
-//        super(series);
-//    }
-//
-//    @Override
-//    protected Boolean calculate(int index) {
-//        if (index < 1) {
-//            // Harami is a 2-candle pattern
-//            return false;
-//        }
-//        Bar prevBar = getBarSeries().getBar(index - 1);
-//        Bar currBar = getBarSeries().getBar(index);
-//        if (prevBar.isBearish() && currBar.isBullish()) {
-//            final Num prevOpenPrice = prevBar.openPrice();
-//            final Num prevClosePrice = prevBar.closePrice();
-//            final Num currOpenPrice = currBar.openPrice();
-//            final Num currClosePrice = currBar.closePrice();
-//            return currOpenPrice.isLessThan(prevOpenPrice) && currOpenPrice.isGreaterThan(prevClosePrice)
-//                    && currClosePrice.isLessThan(prevOpenPrice) && currClosePrice.isGreaterThan(prevClosePrice);
-//        }
-//        return false;
-//    }
-//
-//    @Override
-//    public int getUnstableBars() {
-//        return 0;
-//    }
-//}
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2023 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.candles;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.ta4j.core.Bar;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.indicators.AbstractIndicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * Bearish Harami pattern indicator.
+ *
+ * @see <a href="http://www.investopedia.com/terms/b/bullishharami.asp">
+ *     http://www.investopedia.com/terms/b/bullishharami.asp</a>
+ */
+public class BullishHaramiIndicator extends AbstractIndicator<Boolean> {
+
+  private ZonedDateTime currentTick = ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.systemDefault());
+  private Boolean value;
+  private Bar previousBar;
+
+
+  /**
+   * Constructor.
+   *
+   * @param series the bar series
+   */
+  public BullishHaramiIndicator(final BarSeries series) {
+    super(series);
+  }
+
+
+  protected Boolean calculate() {
+    if (this.value == null) {
+      this.previousBar = getBarSeries().getBar();
+      // Harami is a 2-candle pattern
+      return false;
+    }
+
+    final Bar prevBar = this.previousBar;
+    final Bar currBar = getBarSeries().getBar();
+    this.previousBar = currBar;
+
+    if (prevBar.isBearish() && currBar.isBullish()) {
+      final Num prevOpenPrice = prevBar.openPrice();
+      final Num prevClosePrice = prevBar.closePrice();
+      final Num currOpenPrice = currBar.openPrice();
+      final Num currClosePrice = currBar.closePrice();
+      return currOpenPrice.isLessThan(prevOpenPrice)
+             && currOpenPrice.isGreaterThan(prevClosePrice)
+             && currClosePrice.isLessThan(prevOpenPrice)
+             && currClosePrice.isGreaterThan(prevClosePrice);
+    }
+    return false;
+  }
+
+
+  @Override
+  public Boolean getValue() {
+    return this.value;
+  }
+
+
+  @Override
+  public void refresh(final ZonedDateTime tick) {
+    if (tick.isAfter(this.currentTick)) {
+      this.value = calculate();
+      this.currentTick = tick;
+    }
+  }
+
+
+  @Override
+  public boolean isStable() {
+    return this.value != null;
+  }
+}
