10658233ba8ae396533e29ebf68b6c91f558c13b
Lukáš Kvídera <kvideral@qwsome.eu>
2024-05-19 15:07:40 +0200

    Enabled BearishHaramiIndicator

--- ta4j-core/src/test/java/org/ta4j/core/indicators/candles/BearishHaramiIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/candles/BearishHaramiIndicatorTest.java
@@ -1,65 +1,71 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core.indicators.candles;
-//
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.ta4j.core.BarSeries;
-//import org.ta4j.core.indicators.Indicator;
-//import org.ta4j.core.indicators.AbstractIndicatorTest;
-//import org.ta4j.core.mocks.MockBarSeriesBuilder;
-//import org.ta4j.core.num.Num;
-//import org.ta4j.core.num.NumFactory;
-//
-//public class BearishHaramiIndicatorTest extends AbstractIndicatorTest<Indicator<Boolean>, Num> {
-//
-//    private BarSeries series;
-//
-//    public BearishHaramiIndicatorTest(NumFactory numFactory) {
-//        super(numFactory);
-//    }
-//
-//    @Before
-//    public void setUp() {
-//        series = new MockBarSeriesBuilder().withNumFactory(numFactory).build();
-//        series.barBuilder().openPrice(10).closePrice(18).highPrice(20).lowPrice(10).add();
-//        series.barBuilder().openPrice(15).closePrice(18).highPrice(19).lowPrice(14).add();
-//        series.barBuilder().openPrice(17).closePrice(16).highPrice(19).lowPrice(15).add();
-//        series.barBuilder().openPrice(15).closePrice(11).highPrice(15).lowPrice(8).add();
-//        series.barBuilder().openPrice(11).closePrice(12).highPrice(12).lowPrice(10).add();
-//    }
-//
-//    @Test
-//    public void getValue() {
-//        var bhp = new BearishHaramiIndicator(series);
-//        assertFalse(bhp.getValue(0));
-//        assertFalse(bhp.getValue(1));
-//        assertTrue(bhp.getValue(2));
-//        assertFalse(bhp.getValue(3));
-//        assertFalse(bhp.getValue(4));
-//    }
-//}
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.candles;
+
+import static org.ta4j.core.TestUtils.assertNextFalse;
+import static org.ta4j.core.TestUtils.assertNextTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.ta4j.core.MockStrategy;
+import org.ta4j.core.backtest.BacktestBarSeries;
+import org.ta4j.core.indicators.AbstractIndicatorTest;
+import org.ta4j.core.indicators.Indicator;
+import org.ta4j.core.mocks.MockBarSeriesBuilder;
+import org.ta4j.core.num.Num;
+import org.ta4j.core.num.NumFactory;
+
+public class BearishHaramiIndicatorTest extends AbstractIndicatorTest<Indicator<Boolean>, Num> {
+
+  private BacktestBarSeries series;
+
+
+  public BearishHaramiIndicatorTest(final NumFactory numFactory) {
+    super(numFactory);
+  }
+
+
+  @Before
+  public void setUp() {
+    this.series = new MockBarSeriesBuilder().withNumFactory(this.numFactory).build();
+    this.series.barBuilder().openPrice(10).closePrice(18).highPrice(20).lowPrice(10).add();
+    this.series.barBuilder().openPrice(15).closePrice(18).highPrice(19).lowPrice(14).add();
+    this.series.barBuilder().openPrice(17).closePrice(16).highPrice(19).lowPrice(15).add();
+    this.series.barBuilder().openPrice(15).closePrice(11).highPrice(15).lowPrice(8).add();
+    this.series.barBuilder().openPrice(11).closePrice(12).highPrice(12).lowPrice(10).add();
+  }
+
+
+  @Test
+  public void getValue() {
+    final var bhp = new BearishHaramiIndicator(this.series);
+    this.series.replaceStrategy(new MockStrategy(bhp));
+
+    assertNextFalse(this.series, bhp);
+    assertNextFalse(this.series, bhp);
+    assertNextTrue(this.series, bhp);
+    assertNextFalse(this.series, bhp);
+    assertNextFalse(this.series, bhp);
+  }
+}
