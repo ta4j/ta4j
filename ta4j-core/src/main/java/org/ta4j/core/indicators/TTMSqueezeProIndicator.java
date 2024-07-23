cd61426fbbfef1698a11e4d62d6e6f8a01a581e0
David Pang <davidzhe@gmail.com>
2024-07-23 15:00:44 -0400

    changed to calculating factor shift with true range instead of close price sma

--- ta4j-core/src/main/java/org/ta4j/core/indicators/TTMSqueezeProIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/TTMSqueezeProIndicator.java
@@ -1,19 +1,19 @@
 /**
  * The MIT License (MIT)
- *
+ * <p>
  * Copyright (c) 2017-2023 Ta4j Organization & respective
  * authors (see AUTHORS)
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -30,8 +30,10 @@ import org.ta4j.core.indicators.bollinger.BollingerBandsMiddleIndicator;
 import org.ta4j.core.indicators.bollinger.BollingerBandsUpperIndicator;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
 import org.ta4j.core.indicators.helpers.CombineIndicator;
+import org.ta4j.core.indicators.helpers.TRIndicator;
 import org.ta4j.core.indicators.helpers.TransformIndicator;
 import org.ta4j.core.indicators.statistics.StandardDeviationIndicator;
+import org.ta4j.core.num.NaN;
 import org.ta4j.core.num.Num;
 
 public class TTMSqueezeProIndicator extends CachedIndicator<Num> {
@@ -48,21 +50,29 @@ public class TTMSqueezeProIndicator extends CachedIndicator<Num> {
 
     private final int barCount;
 
-    public TTMSqueezeProIndicator(BarSeries series, int barCount, double bbLowerBandK, double bbUpperBandK,
-            double shiftFactorHigh, double shiftFactorMid, double shiftFactorLow) {
+    public TTMSqueezeProIndicator(BarSeries series, int barCount) {
+        this(series, barCount, 2, 2, 1, 1.5, 2);
+    }
+
+    public TTMSqueezeProIndicator(BarSeries series, int barCount, Number bbLowerBandK, Number bbUpperBandK,
+                                  Number shiftFactorHigh, Number shiftFactorMid, Number shiftFactorLow) {
         super(series);
 
         ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
         StandardDeviationIndicator closePriceStdDev = new StandardDeviationIndicator(closePrice, barCount);
-        CachedIndicator<Num> closePriceSma = new SMAIndicator(closePrice, barCount);
+        SMAIndicator closePriceSma = new SMAIndicator(closePrice, barCount);
+
+        TRIndicator trueRange = new TRIndicator(series);
+        SMAIndicator trueRangeSma = new SMAIndicator(trueRange, barCount);
+
         BollingerBandsMiddleIndicator midLineBB = new BollingerBandsMiddleIndicator(closePriceSma);
 
         this.upperBandBB = new BollingerBandsUpperIndicator(midLineBB, closePriceStdDev, numOf(bbUpperBandK));
         this.lowerBandBB = new BollingerBandsLowerIndicator(midLineBB, closePriceStdDev, numOf(bbLowerBandK));
 
-        Indicator<Num> shiftHigh = TransformIndicator.multiply(closePriceSma, shiftFactorHigh);
-        Indicator<Num> shiftMid = TransformIndicator.multiply(closePriceSma, shiftFactorMid);
-        Indicator<Num> shiftLow = TransformIndicator.multiply(closePriceSma, shiftFactorLow);
+        Indicator<Num> shiftHigh = TransformIndicator.multiply(trueRangeSma, shiftFactorHigh);
+        Indicator<Num> shiftMid = TransformIndicator.multiply(trueRangeSma, shiftFactorMid);
+        Indicator<Num> shiftLow = TransformIndicator.multiply(trueRangeSma, shiftFactorLow);
 
         this.upperBandKCLow = CombineIndicator.plus(closePriceSma, shiftLow);
         this.lowerBandKCLow = CombineIndicator.minus(closePriceSma, shiftLow);
@@ -75,17 +85,21 @@ public class TTMSqueezeProIndicator extends CachedIndicator<Num> {
 
     @Override
     protected Num calculate(int index) {
-        boolean presqueeze = lowerBandBB.getValue(index).isGreaterThan(lowerBandKCLow.getValue(index))
+        if (index < this.getUnstableBars()) {
+            return NaN.NaN;
+        }
+
+        boolean preSqueeze = lowerBandBB.getValue(index).isGreaterThan(lowerBandKCLow.getValue(index))
                 && upperBandBB.getValue(index).isLessThan(upperBandKCLow.getValue(index));
         boolean originalSqueeze = lowerBandBB.getValue(index).isGreaterThan(lowerBandKCMid.getValue(index))
                 && upperBandBB.getValue(index).isLessThan(upperBandKCMid.getValue(index));
-        boolean extrSqueeze = lowerBandBB.getValue(index).isGreaterThan(lowerBandKCHigh.getValue(index))
+        boolean extraSqueeze = lowerBandBB.getValue(index).isGreaterThan(lowerBandKCHigh.getValue(index))
                 && upperBandBB.getValue(index).isLessThan(upperBandKCHigh.getValue(index));
 
-        if (presqueeze || originalSqueeze || extrSqueeze) {
-            return numOf(1);
+        if (preSqueeze || originalSqueeze || extraSqueeze) {
+            return one();
         } else {
-            return numOf(0);
+            return zero();
         }
     }
 
