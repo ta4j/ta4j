f77daafe54718e301364c2ae2085ca4f1a97ae7c
Lukáš Kvídera <kvideral@qwsome.eu>
2024-09-07 17:11:27 +0200

    Enabled CorrelationCoefficientIndicator

--- ta4j-core/src/test/java/org/ta4j/core/indicators/statistics/CorrelationCoefficientIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/statistics/CorrelationCoefficientIndicatorTest.java
@@ -1,106 +1,103 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core.indicators.statistics;
-//
-//import static org.junit.Assert.assertTrue;
-//import static org.ta4j.core.TestUtils.assertNumEquals;
-//
-//import java.time.ZonedDateTime;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.ta4j.core.BarSeries;
-//import org.ta4j.core.indicators.Indicator;
-//import org.ta4j.core.indicators.AbstractIndicatorTest;
-//import org.ta4j.core.indicators.candles.price.ClosePriceIndicator;
-//import org.ta4j.core.indicators.helpers.RunningVolumeSumIndicator;
-//import org.ta4j.core.mocks.MockBarSeriesBuilder;
-//import org.ta4j.core.num.Num;
-//import org.ta4j.core.num.NumFactory;
-//
-//public class CorrelationCoefficientIndicatorTest extends AbstractIndicatorTest<Indicator<Num>, Num> {
-//
-//    private Indicator<Num> close, volume;
-//
-//    public CorrelationCoefficientIndicatorTest(NumFactory numFactory) {
-//        super(numFactory);
-//    }
-//
-//    @Before
-//    public void setUp() {
-//        BarSeries data = new MockBarSeriesBuilder().withNumFactory(numFactory).build();
-//        int i = 20;
-//        // close, volume
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(6).volume(100).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(7).volume(105).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(9).volume(130).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(12).volume(160).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(11).volume(150).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(10).volume(130).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(11).volume(95).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(13).volume(120).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(15).volume(180).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(12).volume(160).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(8).volume(150).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(4).volume(200).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(3).volume(150).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(4).volume(85).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(3).volume(70).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(5).volume(90).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(8).volume(100).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(9).volume(95).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i--)).closePrice(11).volume(110).add();
-//        data.barBuilder().endTime(ZonedDateTime.now().minusSeconds(i)).closePrice(10).volume(95).add();
-//
-//        close = new ClosePriceIndicator(data);
-//        volume = new VolumeIndicator(data, 2);
-//    }
-//
-//    @Test
-//    public void usingBarCount5UsingClosePriceAndVolume() {
-//        var coef = new CorrelationCoefficientIndicator(close, volume, 5);
-//
-//        assertTrue(coef.getValue(0).isNaN());
-//
-//        assertNumEquals(1, coef.getValue(1));
-//        assertNumEquals(0.8773, coef.getValue(2));
-//        assertNumEquals(0.9073, coef.getValue(3));
-//        assertNumEquals(0.9219, coef.getValue(4));
-//        assertNumEquals(0.9205, coef.getValue(5));
-//        assertNumEquals(0.4565, coef.getValue(6));
-//        assertNumEquals(-0.4622, coef.getValue(7));
-//        assertNumEquals(0.05747, coef.getValue(8));
-//        assertNumEquals(0.1442, coef.getValue(9));
-//        assertNumEquals(-0.1263, coef.getValue(10));
-//        assertNumEquals(-0.5345, coef.getValue(11));
-//        assertNumEquals(-0.7275, coef.getValue(12));
-//        assertNumEquals(0.1676, coef.getValue(13));
-//        assertNumEquals(0.2506, coef.getValue(14));
-//        assertNumEquals(-0.2938, coef.getValue(15));
-//        assertNumEquals(-0.3586, coef.getValue(16));
-//        assertNumEquals(0.1713, coef.getValue(17));
-//        assertNumEquals(0.9841, coef.getValue(18));
-//        assertNumEquals(0.9799, coef.getValue(19));
-//    }
-//}
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.statistics;
+
+import static org.ta4j.core.TestUtils.assertNext;
+import static org.ta4j.core.TestUtils.fastForward;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.ta4j.core.MockStrategy;
+import org.ta4j.core.backtest.BacktestBarSeries;
+import org.ta4j.core.indicators.AbstractIndicatorTest;
+import org.ta4j.core.indicators.numeric.NumericIndicator;
+import org.ta4j.core.mocks.MockBarSeriesBuilder;
+import org.ta4j.core.num.Num;
+import org.ta4j.core.num.NumFactory;
+
+public class CorrelationCoefficientIndicatorTest extends AbstractIndicatorTest<Num> {
+
+  private NumericIndicator close;
+  private NumericIndicator volume;
+  private BacktestBarSeries data;
+
+
+  public CorrelationCoefficientIndicatorTest(final NumFactory numFactory) {
+    super(numFactory);
+  }
+
+
+  @Before
+  public void setUp() {
+    this.data = new MockBarSeriesBuilder().withNumFactory(this.numFactory).build();
+    this.data.barBuilder().closePrice(6).volume(100).add();
+    this.data.barBuilder().closePrice(7).volume(105).add();
+    this.data.barBuilder().closePrice(9).volume(130).add();
+    this.data.barBuilder().closePrice(12).volume(160).add();
+    this.data.barBuilder().closePrice(11).volume(150).add();
+    this.data.barBuilder().closePrice(10).volume(130).add();
+    this.data.barBuilder().closePrice(11).volume(95).add();
+    this.data.barBuilder().closePrice(13).volume(120).add();
+    this.data.barBuilder().closePrice(15).volume(180).add();
+    this.data.barBuilder().closePrice(12).volume(160).add();
+    this.data.barBuilder().closePrice(8).volume(150).add();
+    this.data.barBuilder().closePrice(4).volume(200).add();
+    this.data.barBuilder().closePrice(3).volume(150).add();
+    this.data.barBuilder().closePrice(4).volume(85).add();
+    this.data.barBuilder().closePrice(3).volume(70).add();
+    this.data.barBuilder().closePrice(5).volume(90).add();
+    this.data.barBuilder().closePrice(8).volume(100).add();
+    this.data.barBuilder().closePrice(9).volume(95).add();
+    this.data.barBuilder().closePrice(11).volume(110).add();
+    this.data.barBuilder().closePrice(10).volume(95).add();
+
+    this.close = NumericIndicator.closePrice(this.data);
+    this.volume = NumericIndicator.volume(this.data);
+  }
+
+
+  @Test
+  public void usingBarCount5UsingClosePriceAndVolume() {
+    final var coef = new CorrelationCoefficientIndicator(this.close, this.volume, 5);
+    this.data.replaceStrategy(new MockStrategy(coef));
+
+    fastForward(this.data, 5);
+    assertNext(this.data, 0.7977, coef);
+    assertNext(this.data, 0.7842, coef);
+    assertNext(this.data, 0.2536, coef);
+    assertNext(this.data, 0.0548, coef);
+    assertNext(this.data, 0.4841, coef);
+    assertNext(this.data, 0.5219, coef);
+    assertNext(this.data, 0.2436, coef);
+    assertNext(this.data, -0.3662, coef);
+    assertNext(this.data, 0.02518, coef);
+    assertNext(this.data, 0.1169, coef);
+    assertNext(this.data, 0.2139, coef);
+    assertNext(this.data, -0.0481, coef);
+    assertNext(this.data, -0.0853, coef);
+    assertNext(this.data, 0.6913, coef);
+    assertNext(this.data, 0.7473, coef);
+    assertNext(this.data, 0.5843, coef);
+  }
+}
