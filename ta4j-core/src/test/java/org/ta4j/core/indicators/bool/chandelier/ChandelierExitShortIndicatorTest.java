21dd6fa0524b1b794999f505e9f5020c490d0cc0
Lukáš Kvídera <kvideral@qwsome.eu>
2024-10-26 18:42:51 +0200

    Enabled ChandelierExitShortIndicator

---
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/bool/chandelier/ChandelierExitShortIndicatorTest.java
@@ -0,0 +1,74 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.bool.chandelier;
+
+import static org.ta4j.core.TestUtils.assertNextFalse;
+import static org.ta4j.core.TestUtils.assertNextTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.ta4j.core.MockStrategy;
+import org.ta4j.core.backtest.BacktestBarSeries;
+import org.ta4j.core.indicators.AbstractIndicatorTest;
+import org.ta4j.core.mocks.MockBarSeriesBuilder;
+import org.ta4j.core.num.Num;
+import org.ta4j.core.num.NumFactory;
+
+public class ChandelierExitShortIndicatorTest extends AbstractIndicatorTest<Num> {
+
+  private BacktestBarSeries data;
+
+
+  public ChandelierExitShortIndicatorTest(final NumFactory numFactory) {
+    super(numFactory);
+  }
+
+
+  @Before
+  public void setUp() {
+    this.data = new MockBarSeriesBuilder().withNumFactory(this.numFactory).build();
+    this.data.barBuilder().lowPrice(102).highPrice(101).closePrice(100).add();
+    this.data.barBuilder().lowPrice(104).highPrice(106).closePrice(105).add();
+    this.data.barBuilder().lowPrice(105).highPrice(107).closePrice(106).add();
+    this.data.barBuilder().lowPrice(105).highPrice(106).closePrice(105).add();
+    this.data.barBuilder().lowPrice(115).highPrice(120).closePrice(119).add(); // this is out of 3 * atr
+    this.data.barBuilder().lowPrice(103).highPrice(105).closePrice(104).add();
+    this.data.barBuilder().lowPrice(105).highPrice(107).closePrice(106).add();
+  }
+
+
+  @Test
+  public void massIndexUsing3And8BarCounts() {
+    final var cel = new ChandelierExitShortIndicator(this.data, 5, 2);
+    this.data.replaceStrategy(new MockStrategy(cel));
+
+    assertNextFalse(this.data, cel);
+    assertNextFalse(this.data, cel);
+    assertNextFalse(this.data, cel);
+    assertNextFalse(this.data, cel);
+    assertNextTrue(this.data, cel);
+    assertNextFalse(this.data, cel);
+    assertNextFalse(this.data, cel);
+  }
+}
