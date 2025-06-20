5e25ef841a1318595b86ec3e727fe57d3758be6c
Lukáš Kvídera <kvideral@qwsome.eu>
2025-06-20 07:19:33 +0200

    #1152 Fixed License header

--- ta4j-core/src/main/java/org/ta4j/core/indicators/statistics/Type.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/statistics/Type.java
@@ -1,8 +1,7 @@
-
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * Copyright (c) 2017-2025 Ta4j Organization & respective
  * authors (see AUTHORS)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -22,7 +21,6 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 package org.ta4j.core.indicators.statistics;
 
 public enum Type {
