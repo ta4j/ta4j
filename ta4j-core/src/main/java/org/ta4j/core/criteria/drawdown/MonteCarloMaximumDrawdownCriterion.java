29a4ba0dac714d9a50ffcb90e5d3924703fa1a44
francescopeloi <francesco.peloi@gmail.com>
2025-08-31 20:21:51 +0200

    better comment

--- ta4j-core/src/main/java/org/ta4j/core/criteria/drawdown/MonteCarloMaximumDrawdownCriterion.java
+++ ta4j-core/src/main/java/org/ta4j/core/criteria/drawdown/MonteCarloMaximumDrawdownCriterion.java
@@ -44,7 +44,7 @@ import org.ta4j.core.num.Num;
 public class MonteCarloMaximumDrawdownCriterion extends AbstractAnalysisCriterion {
 
     /**
-     * Statistic to return from the bootstrap distribution.
+     * Defines which summary value to return from the simulated drawdowns.
      */
     public enum Statistic {
         MEDIAN, P95, P99, MEAN, MIN, MAX
