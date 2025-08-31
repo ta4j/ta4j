36791d3266f27399b60476c4bb80613503b7997e
francescopeloi <francesco.peloi@gmail.com>
2025-08-31 19:21:46 +0200

    moved drawdown classes into its own package

--- ta4j-core/src/test/java/org/ta4j/core/criteria/drawdown/ReturnOverMaxDrawdownCriterionTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/criteria/drawdown/ReturnOverMaxDrawdownCriterionTest.java
@@ -21,7 +21,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package org.ta4j.core.criteria;
+package org.ta4j.core.criteria.drawdown;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -33,7 +33,7 @@ import org.ta4j.core.AnalysisCriterion;
 import org.ta4j.core.BaseTradingRecord;
 import org.ta4j.core.Position;
 import org.ta4j.core.Trade;
-import org.ta4j.core.criteria.drawdown.ReturnOverMaxDrawdownCriterion;
+import org.ta4j.core.criteria.AbstractCriterionTest;
 import org.ta4j.core.mocks.MockBarSeriesBuilder;
 import org.ta4j.core.num.NumFactory;
 
