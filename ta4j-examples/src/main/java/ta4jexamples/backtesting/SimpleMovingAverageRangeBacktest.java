4d44159009d8fe7093395c500a87f6fbcda5a17a
David Pang <davidzhe@gmail.com>
2025-11-16 10:27:08 -0500

    Update logging levels in configuration and examples; change root logger level from debug to info and adjust log statements from info to debug for better log management.

--- ta4j-examples/src/main/java/ta4jexamples/backtesting/SimpleMovingAverageRangeBacktest.java
+++ ta4j-examples/src/main/java/ta4jexamples/backtesting/SimpleMovingAverageRangeBacktest.java
@@ -62,7 +62,7 @@ public class SimpleMovingAverageRangeBacktest {
         List<TradingStatement> tradingStatements = backtestExecutor.execute(strategies, DecimalNum.valueOf(50),
                 Trade.TradeType.BUY);
 
-        LOG.info(printReport(tradingStatements));
+        LOG.debug(printReport(tradingStatements));
     }
 
     private static Rule createEntryRule(BarSeries series, int barCount) {
