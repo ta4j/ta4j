16d39fdbed95b9e1a735f85527a3e570b0ff25ed
btezergil <btezergil@gmail.com>
2026-02-01 12:47:27 +0300

    Review changes

--- ta4j-core/src/main/java/org/ta4j/core/rules/AndWithThresholdRule.java
+++ ta4j-core/src/main/java/org/ta4j/core/rules/AndWithThresholdRule.java
@@ -1,25 +1,5 @@
 /*
- * The MIT License (MIT)
- *
- * Copyright (c) 2017-2025 Ta4j Organization & respective
- * authors (see AUTHORS)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * SPDX-License-Identifier: MIT
  */
 package org.ta4j.core.rules;
 
@@ -37,6 +17,8 @@ import org.ta4j.core.TradingRecord;
  * <p>
  * <b>Warning:</b> The second rule is not tested if the first rule is not
  * satisfied.
+ *
+ * @since 0.22.2
  */
 public class AndWithThresholdRule extends AbstractRule {
 
@@ -47,7 +29,7 @@ public class AndWithThresholdRule extends AbstractRule {
      * The number of bars in which the rule must be satisfied. The current index is
      * included.
      */
-    private int threshold;
+    private final int threshold;
 
     /**
      * Constructor.
--- ta4j-core/src/main/java/org/ta4j/core/rules/OrWithThresholdRule.java
+++ ta4j-core/src/main/java/org/ta4j/core/rules/OrWithThresholdRule.java
@@ -1,25 +1,5 @@
 /*
- * The MIT License (MIT)
- *
- * Copyright (c) 2017-2025 Ta4j Organization & respective
- * authors (see AUTHORS)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * SPDX-License-Identifier: MIT
  */
 package org.ta4j.core.rules;
 
@@ -36,6 +16,8 @@ import org.ta4j.core.TradingRecord;
  *
  * <p>
  * <b>Warning:</b> The second rule is not tested if the first rule is satisfied.
+ *
+ * @since 0.22.2
  */
 public class OrWithThresholdRule extends AbstractRule {
 
@@ -46,7 +28,7 @@ public class OrWithThresholdRule extends AbstractRule {
      * The number of bars in which the rule must be satisfied. The current index is
      * included.
      */
-    private int threshold = 0;
+    private final int threshold;
 
     /**
      * Constructor.
