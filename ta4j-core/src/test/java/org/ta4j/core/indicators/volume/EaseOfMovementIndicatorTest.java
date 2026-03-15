9929f23a61940f7aeb764b29ee2ad8f1f073d85f
David Pang <davidzhe@gmail.com>
2026-03-15 13:10:23 -0400

    refactor: inline volume spreadsheet scenarios in tests

--- ta4j-core/src/test/java/org/ta4j/core/indicators/volume/EaseOfMovementIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/volume/EaseOfMovementIndicatorTest.java
@@ -31,6 +31,24 @@ public class EaseOfMovementIndicatorTest extends AbstractIndicatorTest<BarSeries
 
     private static final double[] SIDEWAYS_EXPECTED = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
+    private static final double[][] BULLISH_OHLCV = { { 100, 102, 103, 99, 1000 }, { 102, 104, 105, 101, 1100 },
+            { 104, 107, 108, 103, 1200 }, { 107, 109, 110, 106, 1300 }, { 109, 112, 113, 108, 1400 },
+            { 112, 114, 115, 111, 1500 }, { 114, 117, 118, 113, 1600 }, { 117, 119, 120, 116, 1700 },
+            { 119, 121, 122, 118, 1800 }, { 121, 124, 125, 120, 1900 }, { 124, 126, 127, 123, 2000 },
+            { 126, 129, 130, 125, 2100 } };
+
+    private static final double[][] BEARISH_OHLCV = { { 130, 128, 131, 127, 1050 }, { 128, 126, 129, 125, 1150 },
+            { 126, 123, 127, 122, 1250 }, { 123, 121, 124, 120, 1350 }, { 121, 118, 122, 117, 1450 },
+            { 118, 116, 119, 115, 1550 }, { 116, 113, 117, 112, 1650 }, { 113, 111, 114, 110, 1750 },
+            { 111, 109, 112, 108, 1850 }, { 109, 106, 110, 105, 1950 }, { 106, 104, 107, 103, 2050 },
+            { 104, 101, 105, 100, 2150 } };
+
+    private static final double[][] SIDEWAYS_OHLCV = { { 100, 101, 102, 99, 1000 }, { 101, 100, 102, 99, 980 },
+            { 100, 101, 102, 99, 1020 }, { 101, 100, 102, 99, 1000 }, { 100, 101, 102, 99, 990 },
+            { 101, 100, 102, 99, 1010 }, { 100, 101, 102, 99, 995 }, { 101, 100, 102, 99, 1005 },
+            { 100, 101, 102, 99, 1000 }, { 101, 100, 102, 99, 1002 }, { 100, 101, 102, 99, 998 },
+            { 101, 100, 102, 99, 1001 } };
+
     public EaseOfMovementIndicatorTest(final NumFactory numFactory) {
         super(numFactory);
     }
@@ -40,14 +58,14 @@ public class EaseOfMovementIndicatorTest extends AbstractIndicatorTest<BarSeries
         // Spreadsheet references were generated with:
         // emv1(i) = distanceMoved(i) / boxRatio(i), EMV(i) = SMA(emv1, 3)
         // using volumeDivisor = 1000.
-        assertScenarioMatches(VolumeSpreadsheetReferenceScenarios.bullish(numFactory), BULLISH_EXPECTED);
-        assertScenarioMatches(VolumeSpreadsheetReferenceScenarios.bearish(numFactory), BEARISH_EXPECTED);
-        assertScenarioMatches(VolumeSpreadsheetReferenceScenarios.sideways(numFactory), SIDEWAYS_EXPECTED);
+        assertScenarioMatches(bullish(numFactory), BULLISH_EXPECTED);
+        assertScenarioMatches(bearish(numFactory), BEARISH_EXPECTED);
+        assertScenarioMatches(sideways(numFactory), SIDEWAYS_EXPECTED);
     }
 
     @Test
     public void reportsExpectedUnstableBoundary() {
-        final BarSeries series = VolumeSpreadsheetReferenceScenarios.bullish(numFactory);
+        final BarSeries series = bullish(numFactory);
         final EaseOfMovementIndicator indicator = new EaseOfMovementIndicator(series, 3, 1000);
 
         assertThat(indicator.getCountOfUnstableBars()).isEqualTo(3);
@@ -57,7 +75,7 @@ public class EaseOfMovementIndicatorTest extends AbstractIndicatorTest<BarSeries
 
     @Test
     public void throwsForInvalidParameters() {
-        final BarSeries series = VolumeSpreadsheetReferenceScenarios.bullish(numFactory);
+        final BarSeries series = bullish(numFactory);
 
         assertThrows(IllegalArgumentException.class, () -> new EaseOfMovementIndicator(series, 0, 1000));
         assertThrows(IllegalArgumentException.class, () -> new EaseOfMovementIndicator(series, -4, 1000));
@@ -72,7 +90,7 @@ public class EaseOfMovementIndicatorTest extends AbstractIndicatorTest<BarSeries
 
     @Test
     public void sourceIndicatorConstructorMatchesSeriesConstructor() {
-        final BarSeries series = VolumeSpreadsheetReferenceScenarios.bullish(numFactory);
+        final BarSeries series = bullish(numFactory);
         final double volumeDivisor = 1000;
 
         final EaseOfMovementIndicator bySeries = new EaseOfMovementIndicator(series, 3, volumeDivisor);
@@ -86,8 +104,7 @@ public class EaseOfMovementIndicatorTest extends AbstractIndicatorTest<BarSeries
 
     @Test
     public void toStringIncludesConfiguredParameters() {
-        final EaseOfMovementIndicator indicator = new EaseOfMovementIndicator(
-                VolumeSpreadsheetReferenceScenarios.bullish(numFactory), 3, 1000);
+        final EaseOfMovementIndicator indicator = new EaseOfMovementIndicator(bullish(numFactory), 3, 1000);
 
         assertThat(indicator.toString()).contains("barCount: 3").contains("volumeDivisor: 1000");
     }
@@ -95,7 +112,7 @@ public class EaseOfMovementIndicatorTest extends AbstractIndicatorTest<BarSeries
     @Test
     @SuppressWarnings("unchecked")
     public void serializationRoundTripPreservesValuesAndUnstableBars() {
-        final BarSeries series = VolumeSpreadsheetReferenceScenarios.bullish(numFactory);
+        final BarSeries series = bullish(numFactory);
         final EaseOfMovementIndicator original = new EaseOfMovementIndicator(series, 3, 1000);
 
         final String json = original.toJson();
@@ -117,6 +134,34 @@ public class EaseOfMovementIndicatorTest extends AbstractIndicatorTest<BarSeries
 
         assertThat(Num.isNaNOrNull(indicator.getValue(0))).isTrue();
         assertNumEquals(0, indicator.getValue(1));
+    }
+
+    private static BarSeries bullish(final NumFactory numFactory) {
+        return buildSeries(numFactory, BULLISH_OHLCV);
+    }
+
+    private static BarSeries bearish(final NumFactory numFactory) {
+        return buildSeries(numFactory, BEARISH_OHLCV);
+    }
+
+    private static BarSeries sideways(final NumFactory numFactory) {
+        return buildSeries(numFactory, SIDEWAYS_OHLCV);
+    }
+
+    private static BarSeries buildSeries(final NumFactory numFactory, final double[][] ohlcvData) {
+        final BarSeries series = new MockBarSeriesBuilder().withNumFactory(numFactory).build();
+
+        for (double[] row : ohlcvData) {
+            series.barBuilder()
+                    .openPrice(row[0])
+                    .closePrice(row[1])
+                    .highPrice(row[2])
+                    .lowPrice(row[3])
+                    .volume(row[4])
+                    .add();
+        }
+
+        return series;
     }
 
     private void assertScenarioMatches(final BarSeries series, final double[] expectedStableValues) {
--- ta4j-core/src/test/java/org/ta4j/core/indicators/volume/KlingerVolumeOscillatorIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/volume/KlingerVolumeOscillatorIndicatorTest.java
@@ -15,6 +15,7 @@ import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
 import org.ta4j.core.indicators.helpers.HighPriceIndicator;
 import org.ta4j.core.indicators.helpers.LowPriceIndicator;
 import org.ta4j.core.indicators.helpers.VolumeIndicator;
+import org.ta4j.core.mocks.MockBarSeriesBuilder;
 import org.ta4j.core.num.Num;
 import org.ta4j.core.num.NumFactory;
 
@@ -41,6 +42,24 @@ public class KlingerVolumeOscillatorIndicatorTest extends AbstractIndicatorTest<
     private static final double[] SIDEWAYS_250X_EXPECTED = { 146354.166666675, 45538.19444445, 87884.837962975,
             3936.246141975, 58630.674511325, -16201.29511745 };
 
+    private static final double[][] BULLISH_OHLCV = { { 100, 102, 103, 99, 1000 }, { 102, 104, 105, 101, 1100 },
+            { 104, 107, 108, 103, 1200 }, { 107, 109, 110, 106, 1300 }, { 109, 112, 113, 108, 1400 },
+            { 112, 114, 115, 111, 1500 }, { 114, 117, 118, 113, 1600 }, { 117, 119, 120, 116, 1700 },
+            { 119, 121, 122, 118, 1800 }, { 121, 124, 125, 120, 1900 }, { 124, 126, 127, 123, 2000 },
+            { 126, 129, 130, 125, 2100 } };
+
+    private static final double[][] BEARISH_OHLCV = { { 130, 128, 131, 127, 1050 }, { 128, 126, 129, 125, 1150 },
+            { 126, 123, 127, 122, 1250 }, { 123, 121, 124, 120, 1350 }, { 121, 118, 122, 117, 1450 },
+            { 118, 116, 119, 115, 1550 }, { 116, 113, 117, 112, 1650 }, { 113, 111, 114, 110, 1750 },
+            { 111, 109, 112, 108, 1850 }, { 109, 106, 110, 105, 1950 }, { 106, 104, 107, 103, 2050 },
+            { 104, 101, 105, 100, 2150 } };
+
+    private static final double[][] SIDEWAYS_OHLCV = { { 100, 101, 102, 99, 1000 }, { 101, 100, 102, 99, 980 },
+            { 100, 101, 102, 99, 1020 }, { 101, 100, 102, 99, 1000 }, { 100, 101, 102, 99, 990 },
+            { 101, 100, 102, 99, 1010 }, { 100, 101, 102, 99, 995 }, { 101, 100, 102, 99, 1005 },
+            { 100, 101, 102, 99, 1000 }, { 101, 100, 102, 99, 1002 }, { 100, 101, 102, 99, 998 },
+            { 101, 100, 102, 99, 1001 } };
+
     public KlingerVolumeOscillatorIndicatorTest(final NumFactory numFactory) {
         super(numFactory);
     }
@@ -49,23 +68,23 @@ public class KlingerVolumeOscillatorIndicatorTest extends AbstractIndicatorTest<
     public void matchesSpreadsheetReferenceValuesWithDefaultScaleAcrossScenarios() {
         // Spreadsheet references were generated with Klinger VF, EMA(3), EMA(5),
         // and KVO = EMA(3) - EMA(5).
-        assertScenarioMatches(VolumeSpreadsheetReferenceScenarios.bullish(numFactory), BULLISH_EXPECTED);
-        assertScenarioMatches(VolumeSpreadsheetReferenceScenarios.bearish(numFactory), BEARISH_EXPECTED);
-        assertScenarioMatches(VolumeSpreadsheetReferenceScenarios.sideways(numFactory), SIDEWAYS_EXPECTED);
+        assertScenarioMatches(bullish(numFactory), BULLISH_EXPECTED);
+        assertScenarioMatches(bearish(numFactory), BEARISH_EXPECTED);
+        assertScenarioMatches(sideways(numFactory), SIDEWAYS_EXPECTED);
     }
 
     @Test
     public void matchesSpreadsheetReferenceValuesWithCustomScaleAcrossScenarios() {
         // Spreadsheet references were generated using the same input bars and periods,
         // with Klinger VF scale multiplier set to 250.
-        assertScenarioMatches(VolumeSpreadsheetReferenceScenarios.bullish(numFactory), BULLISH_250X_EXPECTED, 250);
-        assertScenarioMatches(VolumeSpreadsheetReferenceScenarios.bearish(numFactory), BEARISH_250X_EXPECTED, 250);
-        assertScenarioMatches(VolumeSpreadsheetReferenceScenarios.sideways(numFactory), SIDEWAYS_250X_EXPECTED, 250);
+        assertScenarioMatches(bullish(numFactory), BULLISH_250X_EXPECTED, 250);
+        assertScenarioMatches(bearish(numFactory), BEARISH_250X_EXPECTED, 250);
+        assertScenarioMatches(sideways(numFactory), SIDEWAYS_250X_EXPECTED, 250);
     }
 
     @Test
     public void supportsSourceIndicatorConstructorWithCustomScale() {
-        final BarSeries series = VolumeSpreadsheetReferenceScenarios.bullish(numFactory);
+        final BarSeries series = bullish(numFactory);
         final KlingerVolumeOscillatorIndicator bySeries = new KlingerVolumeOscillatorIndicator(series, 3, 5, 250);
         final KlingerVolumeOscillatorIndicator byIndicators = new KlingerVolumeOscillatorIndicator(
                 new HighPriceIndicator(series), new LowPriceIndicator(series), new ClosePriceIndicator(series),
@@ -78,7 +97,7 @@ public class KlingerVolumeOscillatorIndicatorTest extends AbstractIndicatorTest<
 
     @Test
     public void reportsExpectedUnstableBoundary() {
-        final BarSeries series = VolumeSpreadsheetReferenceScenarios.bullish(numFactory);
+        final BarSeries series = bullish(numFactory);
         final KlingerVolumeOscillatorIndicator indicator = new KlingerVolumeOscillatorIndicator(series, 3, 5);
 
         assertThat(indicator.getCountOfUnstableBars()).isEqualTo(6);
@@ -88,7 +107,7 @@ public class KlingerVolumeOscillatorIndicatorTest extends AbstractIndicatorTest<
 
     @Test
     public void throwsForInvalidPeriods() {
-        final BarSeries series = VolumeSpreadsheetReferenceScenarios.bullish(numFactory);
+        final BarSeries series = bullish(numFactory);
 
         assertThrows(IllegalArgumentException.class, () -> new KlingerVolumeOscillatorIndicator(series, 0, 5));
         assertThrows(IllegalArgumentException.class, () -> new KlingerVolumeOscillatorIndicator(series, 5, 5));
@@ -97,7 +116,7 @@ public class KlingerVolumeOscillatorIndicatorTest extends AbstractIndicatorTest<
 
     @Test
     public void throwsForInvalidScaleMultiplier() {
-        final BarSeries series = VolumeSpreadsheetReferenceScenarios.bullish(numFactory);
+        final BarSeries series = bullish(numFactory);
 
         assertThrows(IllegalArgumentException.class, () -> new KlingerVolumeOscillatorIndicator(series, 3, 5, null));
         assertThrows(IllegalArgumentException.class, () -> new KlingerVolumeOscillatorIndicator(series, 3, 5, 0));
@@ -110,8 +129,8 @@ public class KlingerVolumeOscillatorIndicatorTest extends AbstractIndicatorTest<
 
     @Test
     public void toStringIncludesConfiguredPeriodsAndScale() {
-        final KlingerVolumeOscillatorIndicator indicator = new KlingerVolumeOscillatorIndicator(
-                VolumeSpreadsheetReferenceScenarios.bullish(numFactory), 3, 5, 250);
+        final KlingerVolumeOscillatorIndicator indicator = new KlingerVolumeOscillatorIndicator(bullish(numFactory), 3,
+                5, 250);
 
         assertThat(indicator.toString()).contains("shortPeriod: 3")
                 .contains("longPeriod: 5")
@@ -121,7 +140,7 @@ public class KlingerVolumeOscillatorIndicatorTest extends AbstractIndicatorTest<
     @Test
     @SuppressWarnings("unchecked")
     public void serializationRoundTripPreservesValuesAndUnstableBars() {
-        final BarSeries series = VolumeSpreadsheetReferenceScenarios.bullish(numFactory);
+        final BarSeries series = bullish(numFactory);
         final KlingerVolumeOscillatorIndicator original = new KlingerVolumeOscillatorIndicator(series, 3, 5, 250);
 
         final String json = original.toJson();
@@ -134,6 +153,34 @@ public class KlingerVolumeOscillatorIndicatorTest extends AbstractIndicatorTest<
         for (int i = series.getBeginIndex(); i <= series.getEndIndex(); i++) {
             assertSameNumOrNaN(original.getValue(i), restoredBase.getValue(i));
         }
+    }
+
+    private static BarSeries bullish(final NumFactory numFactory) {
+        return buildSeries(numFactory, BULLISH_OHLCV);
+    }
+
+    private static BarSeries bearish(final NumFactory numFactory) {
+        return buildSeries(numFactory, BEARISH_OHLCV);
+    }
+
+    private static BarSeries sideways(final NumFactory numFactory) {
+        return buildSeries(numFactory, SIDEWAYS_OHLCV);
+    }
+
+    private static BarSeries buildSeries(final NumFactory numFactory, final double[][] ohlcvData) {
+        final BarSeries series = new MockBarSeriesBuilder().withNumFactory(numFactory).build();
+
+        for (double[] row : ohlcvData) {
+            series.barBuilder()
+                    .openPrice(row[0])
+                    .closePrice(row[1])
+                    .highPrice(row[2])
+                    .lowPrice(row[3])
+                    .volume(row[4])
+                    .add();
+        }
+
+        return series;
     }
 
     private void assertScenarioMatches(final BarSeries series, final double[] expectedStableValues) {
