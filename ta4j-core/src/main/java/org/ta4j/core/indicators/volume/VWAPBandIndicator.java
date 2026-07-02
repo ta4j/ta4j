048388a837507c11aa41f41ba63046ffefda18b7
David Pang <davidzhe@gmail.com>
2026-07-02 19:57:19 -0400

    CF-236 clear VWAP constructor debt

--- ta4j-core/src/main/java/org/ta4j/core/indicators/volume/VWAPBandIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/volume/VWAPBandIndicator.java
@@ -8,6 +8,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import org.ta4j.core.BarSeries;
 import org.ta4j.core.Indicator;
 import org.ta4j.core.indicators.CachedIndicator;
 import org.ta4j.core.indicators.IndicatorUtils;
@@ -55,17 +56,31 @@ public class VWAPBandIndicator extends CachedIndicator<Num> {
      */
     public VWAPBandIndicator(AbstractVWAPIndicator vwapIndicator, Indicator<Num> standardDeviationIndicator,
             Number multiplier, BandType bandType) {
-        super(IndicatorUtils.requireSameSeries(vwapIndicator, standardDeviationIndicator));
-        this.vwapIndicator = vwapIndicator;
-        this.standardDeviationIndicator = standardDeviationIndicator;
-        this.bandType = Objects.requireNonNull(bandType, "bandType must not be null");
-        this.multiplier = getBarSeries().numFactory()
+        this(validatedConfig(vwapIndicator, standardDeviationIndicator, multiplier, bandType));
+    }
+
+    private VWAPBandIndicator(Config config) {
+        super(config.series());
+        this.vwapIndicator = config.vwapIndicator();
+        this.standardDeviationIndicator = config.standardDeviationIndicator();
+        this.bandType = config.bandType();
+        this.multiplier = config.multiplier();
+        this.band = config.band();
+    }
+
+    private static Config validatedConfig(AbstractVWAPIndicator vwapIndicator,
+            Indicator<Num> standardDeviationIndicator, Number multiplier, BandType bandType) {
+        BarSeries series = IndicatorUtils.requireSameSeries(vwapIndicator, standardDeviationIndicator);
+        BandType validatedBandType = Objects.requireNonNull(bandType, "bandType must not be null");
+        Num validatedMultiplier = series.numFactory()
                 .numOf(Objects.requireNonNull(multiplier, "multiplier must not be null"));
-        if (Num.isNaNOrNull(this.multiplier)) {
+        if (Num.isNaNOrNull(validatedMultiplier)) {
             throw new IllegalArgumentException("multiplier must be a valid number");
         }
-        this.band = new BandIndicator(vwapIndicator, standardDeviationIndicator, this.multiplier.getDelegate(),
-                BandIndicator.BandType.valueOf(bandType.name()));
+        BandIndicator band = new BandIndicator(vwapIndicator, standardDeviationIndicator,
+                validatedMultiplier.getDelegate(), BandIndicator.BandType.valueOf(validatedBandType.name()));
+        return new Config(series, vwapIndicator, standardDeviationIndicator, validatedMultiplier, band,
+                validatedBandType);
     }
 
     /**
@@ -78,7 +93,11 @@ public class VWAPBandIndicator extends CachedIndicator<Num> {
      * @since 0.22.3
      */
     public VWAPBandIndicator(AbstractVWAPIndicator vwapIndicator, Number multiplier, BandType bandType) {
-        this(vwapIndicator, new VWAPStandardDeviationIndicator(vwapIndicator), multiplier, bandType);
+        this(validatedConfig(vwapIndicator, multiplier, bandType));
+    }
+
+    private static Config validatedConfig(AbstractVWAPIndicator vwapIndicator, Number multiplier, BandType bandType) {
+        return validatedConfig(vwapIndicator, new VWAPStandardDeviationIndicator(vwapIndicator), multiplier, bandType);
     }
 
     /**
@@ -146,5 +165,9 @@ public class VWAPBandIndicator extends CachedIndicator<Num> {
         } catch (NumberFormatException ex) {
             return raw;
         }
+    }
+
+    private record Config(BarSeries series, AbstractVWAPIndicator vwapIndicator,
+            Indicator<Num> standardDeviationIndicator, Num multiplier, BandIndicator band, BandType bandType) {
     }
 }
