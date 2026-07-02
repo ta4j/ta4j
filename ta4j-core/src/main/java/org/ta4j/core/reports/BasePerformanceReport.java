ecc5ceddf7cff67df69c4745bb869a94f7f63d11
David Pang <davidzhe@gmail.com>
2026-07-02 18:10:27 -0400

    CF-236 clear compare equality debt

--- ta4j-core/src/main/java/org/ta4j/core/reports/BasePerformanceReport.java
+++ ta4j-core/src/main/java/org/ta4j/core/reports/BasePerformanceReport.java
@@ -3,6 +3,8 @@
  */
 package org.ta4j.core.reports;
 
+import java.util.Objects;
+
 import org.ta4j.core.num.Num;
 
 /**
@@ -70,7 +72,8 @@ public class BasePerformanceReport implements PerformanceReport {
     }
 
     /**
-     * Compares this report to another based on total profit/loss.
+     * Compares this report to another based first on total profit/loss, then the
+     * remaining profit/loss metrics as deterministic tie breakers.
      *
      * @param o the other performance report to compare
      * @return a negative integer, zero, or a positive integer as this report's
@@ -81,9 +84,39 @@ public class BasePerformanceReport implements PerformanceReport {
     @Override
     public int compareTo(PerformanceReport o) {
         if (o instanceof BasePerformanceReport bo) {
-            return this.totalProfitLoss.compareTo(bo.totalProfitLoss);
+            int result = this.totalProfitLoss.compareTo(bo.totalProfitLoss);
+            if (result != 0) {
+                return result;
+            }
+            result = this.totalProfitLossPercentage.compareTo(bo.totalProfitLossPercentage);
+            if (result != 0) {
+                return result;
+            }
+            result = this.totalProfit.compareTo(bo.totalProfit);
+            if (result != 0) {
+                return result;
+            }
+            return this.totalLoss.compareTo(bo.totalLoss);
         }
         throw new ClassCastException(this.getClass().getSimpleName() + " instance cannot be compared to object of type "
                 + o.getClass().getSimpleName());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof BasePerformanceReport other)) {
+            return false;
+        }
+        return Objects.equals(totalProfitLoss, other.totalProfitLoss)
+                && Objects.equals(totalProfitLossPercentage, other.totalProfitLossPercentage)
+                && Objects.equals(totalProfit, other.totalProfit) && Objects.equals(totalLoss, other.totalLoss);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(totalProfitLoss, totalProfitLossPercentage, totalProfit, totalLoss);
     }
 }
